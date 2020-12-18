36752e0f03a57f76f4e9df4581ca0890ef51ac7c
Alex Younger <alexanderyounger@gmail.com>
2020-12-18 11:42:52 -0500

    added eslint config, prettier, husky, lint-staged to root. turned off autocrlf in git so windows users can convert line-endings without having them converted back. setup package.json in root. added gitignore.

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+  },
+};
--- backend/app/express.js
+++ backend/app/express.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express from 'express';
 import cookieParser from 'cookie-parser';
 // const passport = require('passport');
 // const cors = require('cors');
@@ -9,7 +9,7 @@ const app = express();
 // app.use(cors({
 //   origin: new RegExp(`${process.env.CORS_ORIGIN_DOMAIN}$`),
 //   credentials: true,
-// })); 
+// }));
 
 app.use(express.json());
 app.use(cookieParser());
@@ -18,8 +18,10 @@ app.use(cookieParser());
 // app.use(routes);
 
 // Error handling
-app.use((err, req, res, next) => {
-  res.status(500).send("Something went wrong with the server!");
+app.use((err, req, res) => {
+  res.status(500).send('Something went wrong with the server!');
+  // remove comment if necessary
+  // eslint-disable-next-line
   console.error(err);
 });
 
