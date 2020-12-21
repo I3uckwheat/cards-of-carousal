aadc51027616190f25aedb053b9e772fe60108cb
Briggs Elsperger <briggse@protonmail.com>
2020-12-20 20:54:37 -0600

    sets up frontend and backend launch scripts

--- backend/start.js
+++ backend/start.js
@@ -1,9 +1,9 @@
 import './env-setup.js';
 
 import app from './app/express.js';
+
+/* ======= Start socket service ======= */
 import './app/socket.js';
 
 // eslint-disable-next-line
 app.listen(process.env.EXPRESS_PORT, () => console.log(`Express listening at http://localhost:${process.env.EXPRESS_PORT}`));
-
-/* ======= Start socket service ======= */
