4d230cf909d583e3cf67738804d167fffb56f87e
Briggs Elsperger <briggse@protonmail.com>
2020-12-22 21:34:00 -0600

    removes un-needed class

--- backend/app/lib/ServerMessage.js
+++
@@ -1,8 +0,0 @@
-import Message from './Message.js';
-
-export default class ServerMessage extends Message {
-  constructor(messageObj) {
-    const messageString = JSON.stringify(messageObj);
-    super(messageString, 'server');
-  }
-}
