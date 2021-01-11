eb3d28650fcb6ede8d43cca40053bdeefc35a336
Alex Younger <alexanderyounger@gmail.com>
2021-01-11 12:31:51 -0500

    cleans up react boilerplate

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import ContextProvider from './context/context';
+import ContextProvider from './contexts/myContext/context';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
