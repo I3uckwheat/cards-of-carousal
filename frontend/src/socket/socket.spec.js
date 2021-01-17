75f9c19fdf23ab45638040750c96c2a2801d151b
Alex Younger <alexanderyounger@gmail.com>
2021-01-17 17:53:48 -0500

    adds socket tests

--- frontend/src/socket/socket.spec.js
+++ frontend/src/socket/socket.spec.js
@@ -2,33 +2,46 @@ import * as socketFunctions from './socket';
 
 const sendMock = jest.fn();
 const closeMock = jest.fn();
+const spyUrl = jest.fn();
 
-class WebSocket {
-  constructor() {
-    this.send = sendMock;
-    this.close = closeMock;
-  }
+function mockSocket(url) {
+  this.url = spyUrl(url);
+  this.send = sendMock;
+  this.close = closeMock;
 }
 
-const attachSocketListeners = jest.fn();
-
 beforeAll(() => {
-  global.WebSocket = WebSocket;
-  global.attachSocketListeners = attachSocketListeners;
+  global.WebSocket = mockSocket;
 });
 
 describe('socket', () => {
-  it('sends a message as host', () => {
+  let testMsg;
+  beforeEach(() => {
+    jest.clearAllMocks();
+    testMsg = { event: 'test', payload: 'test' };
+  });
+  it('sets the url', () => {
     socketFunctions.createLobby();
-    const testMsg = { event: 'test', payload: 'test' };
+    expect(spyUrl).toHaveBeenCalledWith(process.env.REACT_APP_SOCKET_URL);
+  });
+  it('sends a message as host with correct arguments', () => {
+    socketFunctions.createLobby();
     socketFunctions.sendMessage(testMsg);
     expect(sendMock.mock.calls.length).toBe(1);
-    // todo: check argument
+    expect(sendMock.mock.calls[0][0]).toEqual(JSON.stringify(testMsg));
   });
-  it('sends a message as player', () => {
+  it('sends a message as player with correct arguments', () => {
     socketFunctions.joinLobby('TEST');
     socketFunctions.sendMessage({ event: 'test', payload: 'test' });
     expect(sendMock.mock.calls.length).toBe(1);
-    // todo: check argument
+    expect(sendMock.mock.calls[0][0]).toEqual(JSON.stringify(testMsg));
+  });
+  it('closes the socket', () => {
+    socketFunctions.createLobby();
+    socketFunctions.closeSocket();
+    expect(closeMock).toHaveBeenCalledTimes(1);
+    expect(() => socketFunctions.sendMessage(testMsg)).toThrow(
+      'Socket is not connected',
+    );
   });
 });
