5928f86aa8a3b2e6ed2355792307e14656f3981d
Alex Younger <alexanderyounger@gmail.com>
2021-01-20 11:54:22 -0500

    adds newlines between tests

--- frontend/src/socket/socket.spec.js
+++ frontend/src/socket/socket.spec.js
@@ -16,26 +16,33 @@ beforeAll(() => {
 
 describe('socket', () => {
   let testMsg;
+
   beforeEach(() => {
     jest.clearAllMocks();
     testMsg = { event: 'test', payload: 'test' };
   });
+
   it('sets the url', () => {
     socketFunctions.createLobby();
-    expect(spyUrl).toHaveBeenCalledWith(`${process.env.REACT_APP_SOCKET_URL}/lobby`);
+    expect(spyUrl).toHaveBeenCalledWith(
+      `${process.env.REACT_APP_SOCKET_URL}/lobby`,
+    );
   });
+
   it('sends a message as host with correct arguments', () => {
     socketFunctions.createLobby();
     socketFunctions.sendMessage(testMsg);
     expect(sendMock.mock.calls.length).toBe(1);
     expect(sendMock.mock.calls[0][0]).toEqual(JSON.stringify(testMsg));
   });
+
   it('sends a message as player with correct arguments', () => {
     socketFunctions.joinLobby('TEST');
     socketFunctions.sendMessage({ event: 'test', payload: 'test' });
     expect(sendMock.mock.calls.length).toBe(1);
     expect(sendMock.mock.calls[0][0]).toEqual(JSON.stringify(testMsg));
   });
+
   it('closes the socket', () => {
     socketFunctions.createLobby();
     socketFunctions.closeSocket();
