19722fbda7a08afbff398560c582dabbf2172ed3
Grace Schwartz <grace.schwar@gmail.com>
2021-01-31 19:16:40 -0500

    WIP

---
+++ frontend/src/components/JoinCode.js
@@ -0,0 +1,13 @@
+// import React from 'react';
+
+// // My test Stuff
+// function JoinCode(props) {
+//   return (
+//     <div>
+//       <p>Join Code:</p>
+//       <p>{props.joinCodeString}</p>
+//     </div>
+//   );
+// }
+
+// export default JoinCode;
