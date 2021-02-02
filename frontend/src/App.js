2ee1440b01f752b548f9e0c6b3d344eaab362852
sher-s7 <sher.sheikh77@gmail.com>
2021-02-02 01:27:06 -0500

    Display black card in app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import BlackCard from './components/Cards/BlackCard';
 import SocketTest from './components/SocketTest';
 
 function App() {
   return (
     <div className="App">
       <SocketTest />
+      <BlackCard pickCount={2}>
+        &quot;_Prompt_ cards\nformatted with _.&quot;
+        &quot;I want a _ **and** _ sandwich! No corners!&quot;
+      </BlackCard>
     </div>
   );
 }
