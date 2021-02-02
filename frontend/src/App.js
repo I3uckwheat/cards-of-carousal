57cdaceedee0c1864128a3b811c02150390ff8e6
Pedro Klepa <pedro.klepa@gmail.com>
2021-02-02 15:37:54 +0000

    Create basic PlayerList component, render it from App.js and define its propTypes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import SocketTest from './components/SocketTest';
+import PlayerList from './components/PlayerList';
 
 function App() {
+  const players = {
+    playerID1: {
+      name: 'Bender',
+      score: 3,
+      czar: false,
+      submittedCards: [1, 4],
+    },
+    playerID2: {
+      name: 'Bacon',
+      score: 5,
+      czar: true,
+      submittedCards: [],
+    },
+    playerID3: {
+      name: 'Briggs',
+      score: 0,
+      czar: false,
+      submittedCards: [5, 6],
+    },
+  };
+
   return (
     <div className="App">
       <SocketTest />
+
+      <PlayerList players={players} />
     </div>
   );
 }
---
+++ frontend/src/components/PlayerList.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import PropTypes from 'prop-types';
+
+function PlayerList({ players }) {
+  console.log(players);
+
+  return (
+    <div>
+      <h1>hi mom</h1>
+    </div>
+  );
+}
+
+PlayerList.propTypes = {
+  players: PropTypes.objectOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      score: PropTypes.number.isRequired,
+      czar: PropTypes.bool.isRequired,
+      submittedCards: PropTypes.arrayOf(PropTypes.number),
+    }),
+  ).isRequired,
+};
+
+export default PlayerList;
