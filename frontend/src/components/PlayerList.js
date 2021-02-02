3b20c25cc1c8cf2944e6b88133999ff42c97fa9b
Pedro Klepa <pedro.klepa@gmail.com>
2021-02-02 17:36:31 +0000

    Add the overall structure for the component using the incoming players object

--- frontend/src/components/PlayerList.js
+++ frontend/src/components/PlayerList.js
@@ -1,14 +1,33 @@
 import React from 'react';
 
+import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
 function PlayerList({ players }) {
-  console.log(players);
+  const playersArr = Object.values(players);
 
   return (
-    <div>
-      <h1>hi mom</h1>
-    </div>
+    <Container>
+      {playersArr.map((player) => <h1>{player.name}</h1>)}
+
+      <PlayerRow isCzar>
+        <IconMockup isCzar />
+
+        <div className="player-info">
+          <h1>Foo</h1>
+          <span>10</span>
+        </div>
+      </PlayerRow>
+
+      <PlayerRow>
+        <IconMockup />
+
+        <div className="player-info">
+          <h1>bar</h1>
+          <span>5</span>
+        </div>
+      </PlayerRow>
+    </Container>
   );
 }
 
@@ -24,3 +43,53 @@ PlayerList.propTypes = {
 };
 
 export default PlayerList;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  width: 486px;
+  padding: 40px;
+  background-color: #e3e3e3;
+
+  & > div:not(:last-child) {
+    margin-bottom: 24px;
+  }
+`;
+
+const PlayerRow = styled.div`
+  display: flex;
+  width: 100%;
+
+  .player-info {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    flex: 1;
+    margin: 0 12px 0 20px;
+    padding: 0 12px;
+
+    background-color: ${(props) => (props.isCzar ? 'var(--secondary-color)' : 'var(--primary-color)')};
+    border-radius: 4px;
+
+    h1 {
+      text-transform: uppercase;
+      font-size: 24px;
+      font-weight: 900;
+      color: ${(props) => (props.isCzar ? 'var(--primary-color)' : 'var(--secondary-color)')};
+    }
+
+    span {
+      color: ${(props) => (props.isCzar ? 'var(--primary-color)' : 'var(--secondary-color)')};
+    }
+  }
+`;
+
+const IconMockup = styled.div`
+  width: 32px;
+  height: 43px;
+  background-color: ${(props) => (props.isCzar ? 'var(--secondary-color)' : 'var(--primary-color)')};
+  border: 2px solid var(--secondary-color);
+  border-radius: 3px;
+`;
