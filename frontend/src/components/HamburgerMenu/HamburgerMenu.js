5a580e39dc08fac26e482c0d122f60c210a904c6
Sullyman <a.m.sullivan.github@gmail.com>
2021-02-02 23:38:10 -0500

    adds HamburgerMenu button toggle

--- frontend/src/components/HamburgerMenu/HamburgerMenu.js
+++ frontend/src/components/HamburgerMenu/HamburgerMenu.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 
-function HamburgerMenu() {
+function HamburgerMenu(props) {
+  const { isActive } = props;
+  const [activeStatus, setActiveStatus] = useState(isActive);
+
   return (
     <div className="HamburgerMenu">
-      <button type="button">
-        X
+      <button
+        type="button"
+        onClick={() => setActiveStatus(!activeStatus)}
+      >
+        {activeStatus ? 'X' : 'O'}
       </button>
     </div>
   );
 }
+
+HamburgerMenu.propTypes = {
+  isActive: PropTypes.bool.isRequired,
+};
 
 export default HamburgerMenu;
