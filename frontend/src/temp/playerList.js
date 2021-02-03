7270afdccc0f563580deb4859280db8f270ff790
Pedro Klepa <pedro.klepa@gmail.com>
2021-02-03 10:40:33 +0000

    Refactor players object out of App.js and into /src/temp/ in order to keep App.js clean for future features additions

---
+++ frontend/src/temp/playerList.js
@@ -0,0 +1,40 @@
+const players = {
+  playerID1: {
+    name: 'Bender',
+    score: 3,
+    czar: false,
+    submittedCards: [1, 4],
+  },
+  playerID2: {
+    name: 'Bacon',
+    score: 5,
+    czar: true,
+    submittedCards: [],
+  },
+  playerID3: {
+    name: 'Briggs',
+    score: 0,
+    czar: false,
+    submittedCards: [5, 6],
+  },
+  playerID4: {
+    name: 'Pedro',
+    score: 10,
+    czar: false,
+    submittedCards: [],
+  },
+  playerID5: {
+    name: 'Foo',
+    score: 9,
+    czar: false,
+    submittedCards: [2, 5],
+  },
+  playerID6: {
+    name: 'Bar',
+    score: 15,
+    czar: false,
+    submittedCards: [],
+  },
+};
+
+export default players;
