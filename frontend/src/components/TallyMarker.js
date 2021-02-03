fc80ab509e9eb831f40ef393edd685779b4788bc
Pedro Klepa <pedro.klepa@gmail.com>
2021-02-03 10:10:11 +0000

    Remove deprecated tallyCount prop from the parent svg element

--- frontend/src/components/TallyMarker.js
+++ frontend/src/components/TallyMarker.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 function TallyMarker({ isCzar, tallyCount }) {
   return (
-    <svg tallyCount={tallyCount} width="42" height="30" viewBox="0 0 42 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg width="42" height="30" viewBox="0 0 42 30" fill="none" xmlns="http://www.w3.org/2000/svg">
       <g id="ScoreTally">
         {tallyCount >= 1 && <path id="tally1" d="M32.4285 3.19775V29.5701" stroke={isCzar ? 'white' : 'black'} strokeWidth="2" data-testid="tally" />}
         {tallyCount >= 2 && <path id="tally2" d="M25.4439 3.51099L23.2217 29.5966" stroke={isCzar ? 'white' : 'black'} strokeWidth="2" data-testid="tally" />}
