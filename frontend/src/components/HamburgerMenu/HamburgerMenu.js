ad485c5eca0e4aef37310e47955aa545c02817a5
Sullyman <a.m.sullivan.github@gmail.com>
2021-02-02 23:32:31 -0500

    adds basic HamburgerMenu button

---
+++ frontend/src/components/HamburgerMenu/HamburgerMenu.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function HamburgerMenu() {
+  return (
+    <div className="HamburgerMenu">
+      <button type="button">
+        X
+      </button>
+    </div>
+  );
+}
+
+export default HamburgerMenu;
