a1ab221f17f9de05dca5176f0a6c663b6eaab40e
Zach Meyer <zachary.michael.meyer@gmail.com>
2021-02-04 10:52:00 -0500

    Add JSON for packs and initial code for MasterDeck
    Aquired the JSON file from the established source, contains full packs.
    Added the lines of code written out previously to start the MasterDeck.
    Formatted with Prettier, cleaned up with ESLint and then commented out.

---
+++ backend/app/lib/MasterDeck/MasterDeck.js
@@ -0,0 +1,7 @@
+// const packArry = [
+//   { title: 'hello', white: [], black: [] },
+//   { title: 'world', white: [], black: [] },
+//   { title: 'foo', white: [], black: [] },
+// ];
+
+// const packNames = packArry.map((pack) => pack.title);
