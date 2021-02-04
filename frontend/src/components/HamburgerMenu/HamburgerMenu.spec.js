db87e578f48811b275c2f5aa5c1715c2217aeea1
Sullyman <a.m.sullivan.github@gmail.com>
2021-02-04 00:23:45 -0500

    adds tests for HamburgerMenu

---
+++ frontend/src/components/HamburgerMenu/HamburgerMenu.spec.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import HamburgerMenu from './HamburgerMenu';
+
+import inactiveHamburger from './inactiveHamburger.svg';
+import activeHamburger from './activeHamburger.svg';
+
+describe('HamburgerMenu', () => {
+  it('displays inactiveHamburger when isActive is set to false', () => {
+    render(<HamburgerMenu isActive={false} onClick={() => {}} onInactive={() => {}} />);
+
+    expect(screen.getByRole('button')).toHaveStyle(`background-image: url(${inactiveHamburger})`);
+  });
+
+  it('displays activeHamburger when isActive is set to true', () => {
+    render(<HamburgerMenu isActive onClick={() => {}} onInactive={() => {}} />);
+
+    expect(screen.getByRole('button')).toHaveStyle(`background-image: url(${activeHamburger})`);
+  });
+
+  it('displays activeHamburger when isActive is set to false and the button has been clicked', () => {
+    render(<HamburgerMenu isActive={false} onClick={() => {}} onInactive={() => {}} />);
+
+    userEvent.click(screen.getByRole('button'));
+    expect(screen.getByRole('button')).toHaveStyle(`background-image: url(${activeHamburger})`);
+  });
+
+  it('displays inactiveHamburger when isActive is set to true and the button has been clicked', () => {
+    render(<HamburgerMenu isActive onClick={() => {}} onInactive={() => {}} />);
+
+    userEvent.click(screen.getByRole('button'));
+    expect(screen.getByRole('button')).toHaveStyle(`background-image: url(${inactiveHamburger})`);
+  });
+
+  it('calls the onClick callback when clicked', () => {
+    const onClick = jest.fn();
+
+    render(<HamburgerMenu isActive={false} onClick={onClick} onInactive={() => {}} />);
+
+    userEvent.click(screen.getByRole('button'));
+    expect(onClick).toHaveBeenCalledTimes(1);
+  });
+
+  it('calls the onClick callback twice when clicked twice', () => {
+    const onClick = jest.fn();
+
+    render(<HamburgerMenu isActive={false} onClick={onClick} onInactive={() => {}} />);
+
+    userEvent.click(screen.getByRole('button'));
+    userEvent.click(screen.getByRole('button'));
+    expect(onClick).toHaveBeenCalledTimes(2);
+  });
+
+  it('does not call the onInactive callback when toggled to active', () => {
+    const onInactive = jest.fn();
+
+    render(<HamburgerMenu isActive={false} onClick={() => {}} onInactive={onInactive} />);
+
+    userEvent.click(screen.getByRole('button'));
+    expect(onInactive).not.toHaveBeenCalled();
+  });
+
+  it('calls the onInactive callback when toggled to inactive', () => {
+    const onInactive = jest.fn();
+
+    render(<HamburgerMenu isActive onClick={() => {}} onInactive={onInactive} />);
+
+    userEvent.click(screen.getByRole('button'));
+    expect(onInactive).toHaveBeenCalledTimes(1);
+  });
+});
