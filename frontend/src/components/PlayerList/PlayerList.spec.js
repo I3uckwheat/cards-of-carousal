a131f718dd517e9f4e8caa44034788b19cd965ff
Pedro Klepa <pedro.klepa@gmail.com>
2021-02-04 12:46:23 +0000

    Update the remaining tests for the new prop format

--- frontend/src/components/PlayerList/PlayerList.spec.js
+++ frontend/src/components/PlayerList/PlayerList.spec.js
@@ -8,16 +8,16 @@ describe('PlayerList', () => {
     const playerList = {
       players: {
         playerID1: {
-          name: 'Bender',
+          name: 'Foo',
           score: 3,
           czar: false,
           submittedCards: [1, 4],
         },
         playerID2: {
-          name: 'Bacon',
+          name: 'Bar',
           score: 5,
           czar: true,
-          submittedCards: [3, 4],
+          submittedCards: [],
         },
       },
       playersIDs: ['playerID1', 'playerID2'],
@@ -31,75 +31,82 @@ describe('PlayerList', () => {
   });
 
   it('If player has submitted his cards or player is czar, render the icon fully visible', () => {
-    const players = {
-      playerID1: {
-        name: 'Bender',
-        score: 3,
-        czar: false,
-        submittedCards: [1, 4],
+    const playerList = {
+      players: {
+        playerID1: {
+          name: 'Foo',
+          score: 3,
+          czar: false,
+          submittedCards: [1, 4],
+        },
+        playerID2: {
+          name: 'Bar',
+          score: 5,
+          czar: true,
+          submittedCards: [],
+        },
       },
-      playerID2: {
-        name: 'Bacon',
-        score: 5,
-        czar: true,
-        submittedCards: [],
-      },
+      playersIDs: ['playerID1', 'playerID2'],
     };
 
-    render(<PlayerList players={players} />);
+    render(<PlayerList playerList={playerList} />);
 
-    expect(screen.getByTestId(`icon-${players.playerID1.name}`)).toBeVisible();
-    expect(screen.getByTestId(`icon-${players.playerID2.name}`)).toBeVisible();
+    expect(screen.getByTestId(`icon-${playerList.players.playerID1.name}`)).toBeVisible();
+    expect(screen.getByTestId(`icon-${playerList.players.playerID2.name}`)).toBeVisible();
   });
 
   it('If player has NOT submitted his cards, render the icon with visibility hidden', () => {
-    const players = {
-      playerID1: {
-        name: 'Bender',
-        score: 3,
-        czar: false,
-        submittedCards: [],
+    const playerList = {
+      players: {
+        playerID1: {
+          name: 'Foo',
+          score: 3,
+          czar: false,
+          submittedCards: [],
+        },
       },
+      playersIDs: ['playerID1'],
     };
 
-    render(<PlayerList players={players} />);
+    render(<PlayerList playerList={playerList} />);
 
-    expect(screen.getByTestId(`icon-${players.playerID1.name}`)).not.toBeVisible();
+    expect(screen.getByTestId(`icon-${playerList.players.playerID1.name}`)).not.toBeVisible();
   });
 
   it('If player score is higher than 10, display his score by text', () => {
-    const players = {
-      playerID1: {
-        name: 'Bender',
-        score: 15,
-        czar: false,
-        submittedCards: [],
+    const playerList = {
+      players: {
+        playerID1: {
+          name: 'Foo',
+          score: 11,
+          czar: false,
+          submittedCards: [],
+        },
       },
-      playerID2: {
-        name: 'Bender2',
-        score: 11,
-        czar: false,
-        submittedCards: [],
-      },
+      playersIDs: ['playerID1'],
     };
-    render(<PlayerList players={players} />);
 
-    expect(screen.getByText(players.playerID1.score)).toBeInTheDocument();
-    expect(screen.getByText(players.playerID2.score)).toBeInTheDocument();
+    render(<PlayerList playerList={playerList} />);
+
+    expect(screen.getByText(playerList.players.playerID1.score)).toBeInTheDocument();
   });
 
   it('If score is lower than 10, display the correct tally count', () => {
-    const players = {
-      playerID1: {
-        name: 'Bender',
-        score: 7,
-        czar: false,
-        submittedCards: [],
+    const playerList = {
+      players: {
+        playerID1: {
+          name: 'Foo',
+          score: 7,
+          czar: false,
+          submittedCards: [],
+        },
       },
+      playersIDs: ['playerID1'],
     };
-    render(<PlayerList players={players} />);
 
-    expect(screen.getAllByTestId('tally')).toHaveLength(players.playerID1.score);
+    render(<PlayerList playerList={playerList} />);
+
+    expect(screen.getAllByTestId('tally')).toHaveLength(playerList.players.playerID1.score);
   });
 
   // TODO: test('If player is czar, render the its row with the proper colors', () => {})
