11a249d160de073af5082d63dc82755618c0cb0d
Pedro Klepa <pedro.klepa@gmail.com>
2021-02-04 11:38:11 +0000

    Move styled-components out of PlayerList component

--- frontend/src/components/PlayerList/PlayerList.js
+++ frontend/src/components/PlayerList/PlayerList.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
-import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import TallyMarker from '../TallyMarker/TallyMarker';
+
+import { Container, PlayerRow, SmallCardsIcon } from './styles';
 
 const propTypes = {
   players: PropTypes.objectOf(
@@ -54,85 +55,3 @@ function PlayerList({ players }) {
 PlayerList.propTypes = propTypes;
 
 export default PlayerList;
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-
-  width: 486px;
-  padding: 40px;
-  background-color: var(--primary-color);
-
-  position: relative;
-  z-index: 1;
-
-  & > div:not(:last-child) {
-    margin-bottom: 24px;
-  }
-`;
-
-const PlayerRow = styled.div`
-  display: flex;
-  align-items: center;
-  width: 100%;
-
-  .player-info {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-
-    flex: 1;
-    margin: ${(props) => (props.isCzar ? '0 12px 0 26px' : '0 24px 0 26px')};
-    padding: ${(props) => (props.isCzar ? '7px 12px' : '7px 0')};
-    border-bottom: ${(props) => (props.isCzar ? 0 : '1px solid #ccc')};
-    border-radius: ${(props) => (props.isCzar ? '4px' : 0)};
-
-    background-color: ${(props) => (props.isCzar ? 'var(--secondary-color)' : 'var(--primary-color)')};
-
-    h1 {
-      text-transform: uppercase;
-      font-size: 24px;
-      font-weight: 900;
-      letter-spacing: 1px;
-      line-height: 1.5em;
-      color: ${(props) => (props.isCzar ? 'var(--primary-color)' : 'var(--secondary-color)')};
-    }
-
-    span {
-      color: ${(props) => (props.isCzar ? 'var(--primary-color)' : 'var(--secondary-color)')};
-      font-size: 24px;
-      font-weight: 900;
-    }
-  }
-`;
-
-// TODO: Swap this for actual SVG files
-const SmallCardsIcon = styled.div`
-  visibility: ${(props) => (props.showIcon || props.isCzar ? 'visible' : 'hidden')};
-
-  width: 32px;
-  height: 43px;
-  margin: ${(props) => (props.isCzar ? '0' : '0 6px')};
-  background-color: ${(props) => (props.isCzar ? 'var(--secondary-color)' : 'var(--primary-color)')};
-  border: 2px solid var(--secondary-color);
-  border-radius: 3px;
-
-  display: block;
-  position: relative;
-
-  &::before {
-    display: ${(props) => (props.isCzar ? 'none' : 'block')};
-    content: '';
-
-    position: absolute;
-    z-index: -1;
-    top: 5px;
-    left: -12px;
-
-    width: 32px;
-    height: 43px;
-    background-color: var(--primary-color);
-    border: 2px solid var(--secondary-color);
-    border-radius: 3px;
-  }
-`;
