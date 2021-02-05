98f395f104912386047feec9734695af263535f1
Sullyman <a.m.sullivan.github@gmail.com>
2021-02-04 21:31:31 -0500

    Update App to render HamburgerMenu

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import HamburgerMenu from './components/HamburgerMenu/HamburgerMenu';
 import SocketTest from './components/SocketTest';
 
+import './App.css';
+
 function App() {
+  const [showSocketTest, setShowSocketTest] = useState(false);
+  const [showHamburgerMenu, setHamburgerMenu] = useState(false);
+
+  const onClickTest = () => console.log('onClick was called!');
+  const onInactiveTest = () => console.log('onInactive was also called!');
+
   return (
     <div className="App">
-      <SocketTest />
+      <div className="buttons-grid">
+        <button type="button" onClick={() => { setShowSocketTest(!showSocketTest); }}>
+          Show SocketTest
+        </button>
+        <button type="button" onClick={() => { setHamburgerMenu(!showHamburgerMenu); }}>
+          Show HamburgerMenu
+        </button>
+      </div>
+
+      {showSocketTest && <SocketTest />}
+      {showHamburgerMenu
+      && <HamburgerMenu isActive={false} onClick={onClickTest} onInactive={onInactiveTest} />}
     </div>
   );
 }
