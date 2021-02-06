4936cf9eb8b91b5991c941d326bde19eb934f683
Grace Schwartz <grace.schwar@gmail.com>
2021-02-06 16:47:34 -0500

    Refactor styled component formatting
    Reduce from 3 styled componets to 1 that included classes to be used
    with child components

--- frontend/src/components/JoinCode/JoinCode.js
+++ frontend/src/components/JoinCode/JoinCode.js
@@ -2,39 +2,39 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-const Wrapper = styled.div`
+const JoinCodeComponent = styled.div`
   display: flex;
   flex-direction: column;
   line-height : 29px;
-`;
 
-const JoinCodeTitle = styled.p`
-  font-size: 18px;
-  color: var(--secondary-color);
-  background-color: var(--primary-color);
-  font-weight: 700;
-  width: 97px;
-  height: 21px; 
-`;
+  .joinCode-title {
+    font-size: 18px;
+    color: var(--secondary-color);
+    background-color: var(--primary-color);
+    font-weight: 700;
+    width: 97px;
+    height: 21px; 
+  }
 
-const JoinCode = styled.p`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  font-size: 56px;
-  font-weight: bold;
-  color: var(--primary-color);
-  background-color: var(--secondary-color);
-  width: 305px;
-  height: 70px; 
+  .joinCode {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 56px;
+    font-weight: bold;
+    color: var(--primary-color);
+    background-color: var(--secondary-color);
+    width: 305px;
+    height: 70px; 
+  }
 `;
 
 function DisplayJoinCode({ code }) {
   return (
-    <Wrapper>
-      <JoinCodeTitle>JOIN CODE:</JoinCodeTitle>
-      <JoinCode>{ code }</JoinCode>
-    </Wrapper>
+    <JoinCodeComponent>
+      <p className="joinCode-title">JOIN CODE:</p>
+      <p className="joinCode">{ code }</p>
+    </JoinCodeComponent>
   );
 }
 
