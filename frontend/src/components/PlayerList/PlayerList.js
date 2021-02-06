dec22dcae04a6375fa7837d4d256a0d9dc8e6ccc
Briggs Elsperger <briggse@protonmail.com>
2021-02-06 14:25:33 -0600

    Adjust names and locations of TallyMarker and helpers

--- frontend/src/components/PlayerList/PlayerList.js
+++ frontend/src/components/PlayerList/PlayerList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
-import TallyMarkers from '../TallyMarkers/TallyMarkers';
+import TallyMarkers from '../TallyMarker/TallyMarker';
 
 const propTypes = {
   playerList: PropTypes.objectOf(
--- frontend/src/components/TallyMarker/TallyMarker.js
+++ frontend/src/components/TallyMarker/TallyMarker.js
@@ -2,29 +2,43 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
+import TallyMarker from './TallyGroup';
+
 const propTypes = {
+  score: PropTypes.number.isRequired,
   color: PropTypes.oneOf(['primary', 'secondary']).isRequired,
-  tallyCount: PropTypes.number.isRequired,
 };
 
-const StyledPath = styled.path`
-  stroke: ${(props) => (props.color === 'primary' ? 'var(--primary-color)' : 'var(--secondary-color)')};
+const Container = styled.div`
+  display: flex;
 `;
 
-function TallyMarker({ color, tallyCount }) {
+const Text = styled.span`
+  color: ${(props) => (props.color === 'primary' ? 'var(--primary-color)' : 'var(--secondary-color)')};
+  font-size: 24px;
+  font-weight: 900;
+`;
+
+function TallyMarkers({ score, color }) {
+  const displayAsText = score > 10;
+
   return (
-    <svg width="42" height="30" viewBox="0 0 42 30" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <g id="ScoreTally">
-        {tallyCount >= 1 && <StyledPath color={color} d="M32.4285 3.19775V29.5701" strokeWidth="2" data-testid="tally" />}
-        {tallyCount >= 2 && <StyledPath color={color} d="M25.4439 3.51099L23.2217 29.5966" strokeWidth="2" data-testid="tally" />}
-        {tallyCount >= 3 && <StyledPath color={color} d="M16.5548 3.51099L14.3326 29.5966" strokeWidth="2" data-testid="tally" />}
-        {tallyCount >= 4 && <StyledPath color={color} d="M7.66589 3.51099L5.44367 29.5966" strokeWidth="2" data-testid="tally" />}
-        {tallyCount >= 5 && <StyledPath color={color} d="M0.916168 0.599211L40.9161 29.1692" strokeWidth="2" data-testid="tally" />}
-      </g>
-    </svg>
+    <Container>
+      {displayAsText && <Text>{score}</Text>}
+
+      {!displayAsText && score <= 5 && <TallyMarker tallyCount={score} color={color} />}
+
+      {!displayAsText && score > 5
+        && (
+        <>
+          <TallyMarker tallyCount={score - 5} color={color} />
+          <TallyMarker tallyCount={5} color={color} />
+        </>
+        )}
+    </Container>
   );
 }
 
-TallyMarker.propTypes = propTypes;
+TallyMarkers.propTypes = propTypes;
 
-export default TallyMarker;
+export default TallyMarkers;
