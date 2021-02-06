c2610473308f79c9eff955fe375730c01ada1ccd
sher-s7 <sher.sheikh77@gmail.com>
2021-02-06 00:14:55 -0500

    feat: Create function to convert given string to markdown
    Includes edge cases for underscore blanks vs. underscore italics

--- frontend/src/components/Cards/BlackCard.jsx
+++ frontend/src/components/Cards/BlackCard.jsx
@@ -6,14 +6,13 @@ import LayeredCards from './LayeredCards';
 
 const BLANK_LENGTH = 8;
 
-const Card = styled.div`
+const StyledBlackCard = styled.div`
   position: relative;
   width: 390px;
   height: 540px;
   background-color: black;
   color: white;
   border-radius: 18px;
-  font-family: Roboto;
   font-weight: 700;
   font-size: 36px;
   padding: 45px 21px 0 21px;
@@ -34,20 +33,18 @@ const Circled = styled.div`
 `;
 
 function BlackCard({ pickCount, children }) {
-  function parseBlanks() {
-    return children
-      .split(' ')
-      .reduce(
-        (text, str) => (str === '_'
-          ? `${text} ${`\\${str}`.repeat(BLANK_LENGTH)}`
-          : `${text}  ${str}`),
-        '',
-      );
+  // 1. Makes sure newlines are doubled for markdown
+  // 2. Escapes and increases number of underscores displayed for each blank in the text
+  function parseForMarkdown(string) {
+    return string
+      .replace(/\n/g, '\n\n')
+      .replace(/(^|(\s))_(\s)/g, `$1${'\\_'.repeat(BLANK_LENGTH)} `)
+      .replace(/(\s)_(([.,'?!:;()+-])|$)/g, ` ${'\\_'.repeat(BLANK_LENGTH)}$2`);
   }
 
   return (
-    <Card>
-      <Markdown>{parseBlanks(children)}</Markdown>
+    <StyledBlackCard>
+      <Markdown options={{ wrapper: 'div' }}>{parseForMarkdown(children)}</Markdown>
       <div style={{ position: 'absolute', bottom: '18px', left: '18px' }}>
         <LayeredCards />
       </div>
@@ -64,7 +61,7 @@ function BlackCard({ pickCount, children }) {
         <span style={{ fontSize: '24px', marginRight: '7px' }}>PICK</span>
         <Circled>{pickCount}</Circled>
       </div>
-    </Card>
+    </StyledBlackCard>
   );
 }
 
