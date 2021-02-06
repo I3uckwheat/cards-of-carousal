b50dd8b71fa20b3068024a3b3799fc501fd79df1
sher-s7 <sher.sheikh77@gmail.com>
2021-02-06 00:12:03 -0500

    Display example cards

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,19 +3,28 @@ import BlackCard from './components/Cards/BlackCard';
 import SocketTest from './components/SocketTest';
 
 function App() {
+  const ex1 = "Knock, knock.\n\n(Who's there?)\n\n_.";
+  const ex2 = "I left Hamilton because I heard about _'s obsession with _.";
+  const ex3 = 'Hypothesis: _\nExperiment: _\nConclusion: _';
+  const ex4 = 'He alone, who owns the _(s), gains the _(s).';
   return (
     <div className="App">
       <SocketTest />
 
       <div
         style={{
-          position: 'absolute',
-          left: '50%',
-          top: '50%',
-          transform: 'translate(-50%, -50%)',
+          display: 'flex',
+          width: '100%',
+          height: '100vh',
+          justifyContent: 'space-evenly',
+          alignItems: 'center',
+          flexWrap: 'wrap',
         }}
       >
-        <BlackCard pickCount={2}>_Prompt_ cards\nformatted with _.</BlackCard>
+        <BlackCard pickCount={1}>{ex1}</BlackCard>
+        <BlackCard pickCount={2}>{ex2}</BlackCard>
+        <BlackCard pickCount={3}>{ex3}</BlackCard>
+        <BlackCard pickCount={2}>{ex4}</BlackCard>
       </div>
     </div>
   );
