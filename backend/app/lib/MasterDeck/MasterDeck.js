24eadeb1cef4353a09cccdef073bbdac9ad72b83
Zach Meyer <zachary.michael.meyer@gmail.com>
2021-02-06 14:33:06 -0500

    Will add getDeck and associated tests

--- backend/app/lib/MasterDeck/MasterDeck.js
+++ backend/app/lib/MasterDeck/MasterDeck.js
@@ -3,9 +3,27 @@ module.exports = class MasterDeck {
   #packNames;
 
   constructor(cards) {
+    if(!cards) throw new Error(`Card data is missing, got: ${cards}`);
+
     this.#cards = Object.values(cards);
     this.#packNames = this.#cards.map((pack) => pack.name);
   }
 
   getPackNames = () => this.#packNames;
+
+  #singleDeckReducer = (acc, pack) => ({
+    black: [...acc.black, ...pack.black],
+    white: [...acc.white, ...pack.white],
+  })
+
+  getDeck = (packIndexes) => {
+    if (!packIndexes || !Array.isArray(packIndexes)) throw new Error(`Expected an array of pack indexes, got: ${packIndexes}`);
+
+    const selectedPacks = this.#cards.filter((pack, index) => packIndexes.includes(index));
+
+    return selectedPacks.reduce(this.#singleDeckReducer, {
+      black: [],
+      white: [],
+    });
+  }
 };
--- backend/app/lib/MasterDeck/MasterDeck.spec.js
+++ backend/app/lib/MasterDeck/MasterDeck.spec.js
@@ -1,22 +1,159 @@
 const MasterDeck = require('./MasterDeck.js');
 
-const mockCards = {
-  1: {
-    name: 'hello',
-  },
-  2: {
-    name: 'world',
-  },
-  3: {
-    name: 'foo',
-  },
-};
+function makeMockCards() {
+  return {
+    1: {
+      name: 'hello',
+      white: [{ text: 'something' }, { text: 'bippy' }],
+      black: [{ text: 'else' }],
+    },
+    2: {
+      name: 'world',
+      white: [{ text: 'hi' }],
+      black: [{ text: 'bye' }],
+    },
+    3: {
+      name: 'foo',
+      white: [{ text: 'goosebumps' }],
+      black: [{ text: 'are you afraid of the dark' }],
+    },
+  };
+}
+
+// TODO: When some packs have no black cards
+// TODO: When some packs have no white cards
 
 describe('MasterDeck', () => {
+  it('constructs when proper data is passed', () => {
+    expect(() => new MasterDeck(makeMockCards())).not.toThrow();
+  });
+
+  it('throws an error when no data is passed', () => {
+    expect(() => new MasterDeck()).toThrowError('Card data is missing, got: undefined');
+  });
+
   describe('getPackNames', () => {
     it('returns the proper pack names', () => {
+      const mockCards = makeMockCards();
+
       const masterDeck = new MasterDeck(mockCards);
       expect(masterDeck.getPackNames()).toEqual(['hello', 'world', 'foo']);
+    });
+  });
+
+  describe('getDeck', () => {
+    it('returns the proper deck when all packs are selected', () => {
+      const mockCards = makeMockCards();
+
+      const masterDeck = new MasterDeck(mockCards);
+      const packs = masterDeck.getDeck([0, 1, 2]);
+
+      expect(packs).toMatchObject({
+        white: [{ text: 'something' }, { text: 'bippy' }, { text: 'hi' }, { text: 'goosebumps' }],
+        black: [{ text: 'else' }, { text: 'bye' }, { text: 'are you afraid of the dark' }],
+      });
+    });
+
+    it('returns the proper deck when some packs are selected', () => {
+      const mockCards = {
+        1: {
+          name: 'hello',
+          white: [{ text: 'something' }, { text: 'bippy' }],
+          black: [{ text: 'else' }],
+        },
+        2: {
+          name: 'world',
+          white: [{ text: 'hi' }],
+          black: [{ text: 'bye' }],
+        },
+        3: {
+          name: 'foo',
+          white: [{ text: 'goosebumps' }],
+          black: [{ text: 'are you afraid of the dark' }],
+        },
+      };
+
+      const masterDeck = new MasterDeck(mockCards);
+      const packs = masterDeck.getDeck([0, 1]);
+
+      expect(packs).toMatchObject({
+        white: [{ text: 'something' }, { text: 'bippy' }, { text: 'hi' }],
+        black: [{ text: 'else' }, { text: 'bye' }],
+      });
+    });
+
+    it('returns an empty deck when no packs are selected', () => {
+      const mockCards = {
+        1: {
+          name: 'hello',
+          white: [{ text: 'something' }, { text: 'bippy' }],
+          black: [{ text: 'else' }],
+        },
+        2: {
+          name: 'world',
+          white: [{ text: 'hi' }],
+          black: [{ text: 'bye' }],
+        },
+        3: {
+          name: 'foo',
+          white: [{ text: 'goosebumps' }],
+          black: [{ text: 'are you afraid of the dark' }],
+        },
+      };
+
+      const masterDeck = new MasterDeck(mockCards);
+      const packs = masterDeck.getDeck([]);
+
+      expect(packs).toMatchObject({
+        white: [],
+        black: [],
+      });
+    });
+
+    it('thows an error when no parameter is passed', () => {
+      const mockCards = {
+        1: {
+          name: 'hello',
+          white: [{ text: 'something' }, { text: 'bippy' }],
+          black: [{ text: 'else' }],
+        },
+        2: {
+          name: 'world',
+          white: [{ text: 'hi' }],
+          black: [{ text: 'bye' }],
+        },
+        3: {
+          name: 'foo',
+          white: [{ text: 'goosebumps' }],
+          black: [{ text: 'are you afraid of the dark' }],
+        },
+      };
+
+      const masterDeck = new MasterDeck(mockCards);
+      expect(() => masterDeck.getDeck()).toThrowError('Expected an array of pack indexes, got: undefined');
+    });
+
+    it('throws an error when a non-array is passed as a parameter', () => {
+      const mockCards = {
+        1: {
+          name: 'hello',
+          white: [{ text: 'something' }, { text: 'bippy' }],
+          black: [{ text: 'else' }],
+        },
+        2: {
+          name: 'world',
+          white: [{ text: 'hi' }],
+          black: [{ text: 'bye' }],
+        },
+        3: {
+          name: 'foo',
+          white: [{ text: 'goosebumps' }],
+          black: [{ text: 'are you afraid of the dark' }],
+        },
+      };
+
+      const masterDeck = new MasterDeck(mockCards);
+      expect(() => masterDeck.getDeck(42)).toThrowError('Expected an array of pack indexes, got: 42');
     });
   });
 });
