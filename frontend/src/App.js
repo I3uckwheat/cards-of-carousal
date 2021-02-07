9e797b338db57623cbc7a3f8a8b4533869c2c650
Grace Schwartz <grace.schwar@gmail.com>
2021-02-06 19:09:48 -0500

    Add button to toggle components being displayed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import SocketTest from './components/SocketTest';
 import DisplayJoinCode from './components/JoinCode/JoinCode';
 
 function App() {
+  const [showSocketTest, setShowSocketTest] = useState(false);
+  const [showJoinCode, setJoinCode] = useState(false);
+
   return (
     <div className="App">
-      <DisplayJoinCode code="XYA3Z" />
+      <div className="buttons-grid">
+        <button type="button" onClick={() => { setShowSocketTest(!showSocketTest); }}>
+          Show SocketTest
+        </button>
+        <button type="button" onClick={() => { setJoinCode(!showJoinCode); }}>
+          Show JoinCode
+        </button>
+      </div>
+
+      {showSocketTest && <SocketTest />}
+      {showJoinCode && <DisplayJoinCode code="XYA3Z" />}
     </div>
   );
 }
