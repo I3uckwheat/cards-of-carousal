dbd65e22c07cb415072beb3e260da9e369f4df4d
sher-s7 <sher.sheikh77@gmail.com>
2021-02-06 21:47:20 -0500

    Create tests for what should not happen

--- frontend/src/components/Cards/BlackCard.spec.js
+++ frontend/src/components/Cards/BlackCard.spec.js
@@ -27,6 +27,7 @@ describe('BlackCard', () => {
       expect(screen.queryAllByTestId('layerCard')).toHaveLength(2);
     });
   });
+
   describe('text parsing', () => {
     it('properly converts markdown to JSX', () => {
       const string = 'This _is_ a **test**\n\n Here is a *new* __line__';
@@ -35,6 +36,14 @@ describe('BlackCard', () => {
 
       expect(screen.queryAllByText('\n')).toHaveLength(0);
       expect(tree).toMatchSnapshot();
+    });
+
+    it('does not display markdown', () => {
+      render(<BlackCard pickCount={0}># **This** _is_ a test</BlackCard>);
+
+      expect(screen.queryByText('#')).not.toBeInTheDocument();
+      expect(screen.queryByText('**')).not.toBeInTheDocument();
+      expect(screen.queryByText('_')).not.toBeInTheDocument();
     });
 
     it('properly converts single underscores to longer blanks', () => {
@@ -47,6 +56,12 @@ describe('BlackCard', () => {
       render(<BlackCard pickCount={0}>This is a _.</BlackCard>);
 
       expect(screen.getByText(/________./)).toBeInTheDocument();
+    });
+
+    it('does not display single underscores by themselves', () => {
+      render(<BlackCard pickCount={0}>This _ should be long</BlackCard>);
+
+      expect(screen.queryByText(/ _ /)).not.toBeInTheDocument();
     });
   });
 });
