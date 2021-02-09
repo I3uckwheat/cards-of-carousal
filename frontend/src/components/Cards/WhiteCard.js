e22b2ebb73a7cc81355cfc783372f5c55e4469a9
sher-s7 <sher.sheikh77@gmail.com>
2021-02-08 21:00:47 -0500

    Start implementing custom scrollbar

--- frontend/src/components/Cards/WhiteCard.js
+++ frontend/src/components/Cards/WhiteCard.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
+import Scrollbars from 'react-custom-scrollbars';
 import LayeredCards from './LayeredCards';
 
 const propTypes = {
@@ -12,74 +13,82 @@ const defaultProps = {
 };
 
 const StyledWhiteCard = styled.div`
+  position: relative;
+  border: 1px solid black;
+  width: 148px;
+  height: 206px;
+  background-color: white;
+  color: black;
+  border-radius: 7px;
+  font-weight: 500;
+  font-size: ${(props) => (props.shrinkFont ? '11px' : '18px')};
+  overflow-wrap: break-word;
+  line-height: 21px;
+  padding: 17px 10px 0 10px;
+  box-shadow: 0px 4px 12px rgba(0, 0, 0, 0.25);
+
+  .shadow {
     position: relative;
-    border: 1px solid black;
-    width: 148px;
-    height: 206px;
-    background-color: white;
-    color: black;
-    border-radius: 7px;
-    font-weight: 500;
-    font-size: ${(props) => (props.shrinkFont ? '11px' : '18px')};
-    overflow-wrap: break-word;
-    line-height: 21px;
-    padding: 17px 15px 0 15px;
-    box-shadow: 0px 4px 12px rgba(0, 0, 0, 0.25);
+    overflow: hidden;
+  }
 
-    .shadow {
-        position: relative;
-        overflow: hidden;
-    }
+  .shadow:after {
+    content: '';
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    box-shadow: inset 0 -20px 10px -10px white;
+    pointer-events: none;
+  }
 
-    .shadow:after {
-        content: '';
-        width: 100%;
-        height: 100%;
-        position: absolute;
-        top: 0;
-        left: 0;
-        box-shadow: inset 0 -20px 10px -10px white;
-        pointer-events: none;
-    }
+  .whiteCardText {
+    height: 145px !important;
+    width: 108%;
+  }
+
+  .whiteCardText p {
+    padding-bottom: 20px;
+    padding-right: 25px;
+  }
+
+  .layeredCards {
+    position: absolute;
+    bottom: 0px;
+    left: 7px;
+    transform: scale(0.78);
+  }
+
+  @media (min-width: 1025px) {
+    width: 267px;
+    height: 363px;
+    font-size: ${(props) => (props.shrinkFont ? '20px' : '36px')};
+    line-height: 42px;
+    padding: 30px 10px 0 30px;
 
     .whiteCardText {
-        max-height: 145px;
-        padding-right: 17px;
-        width: 108%;
-        overflow-y: auto;
+      height: 280px !important;
     }
-
-    .whiteCardText div {
-        padding-bottom: 20px;
-    }
-
-    .layeredCards {
-        position: absolute;
-        bottom: 0px;
-        left: 7px;
-        transform: scale(0.78);
-    }
-
-    @media (min-width: 1025px) {
-        width: 267px;
-        height: 363px;
-        font-size: ${(props) => (props.shrinkFont ? '20px' : '36px')};
-        line-height: 42px;
-        padding: 30px 27px 0 27px;
-
-        .whiteCardText {
-            max-height: 280px;
-        }
-    }
+  }
 `;
 
 function WhiteCard({ children }) {
   return (
     <StyledWhiteCard shrinkFont={children.length > 75}>
       <div className="shadow">
-        <div className="whiteCardText">
-          <div>{children}</div>
-        </div>
+        <Scrollbars
+          renderThumbVertical={() => (
+            <div style={{
+              backgroundColor: 'black', zIndex: 1, position: 'relative', borderRadius: '15px',
+            }}
+            />
+          )}
+          hideTracksWhenNotNeeded
+          className="whiteCardText"
+        >
+          <p>{children}</p>
+        </Scrollbars>
       </div>
       <div className="layeredCards">
         <LayeredCards />
