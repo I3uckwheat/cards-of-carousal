83fdd517d60631dd0098742282d163483fd35893
sher-s7 <sher.sheikh77@gmail.com>
2021-02-10 21:18:00 -0500

    Add stacked card svg for white card

--- frontend/src/components/Cards/WhiteCard.js
+++ frontend/src/components/Cards/WhiteCard.js
@@ -1,7 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
-import Scrollbars from 'react-custom-scrollbars';
 import LayeredCards from './LayeredCards';
 
 const propTypes = {
@@ -13,104 +12,74 @@ const defaultProps = {
 };
 
 const StyledWhiteCard = styled.div`
-  position: relative;
-  border: 1px solid black;
-  width: 148px;
-  height: 206px;
-  background-color: white;
-  color: black;
-  border-radius: 7px;
-  font-weight: 500;
-  font-size: ${(props) => (props.shrinkFont ? '11px' : '18px')};
-  overflow-wrap: break-word;
-  line-height: 21px;
-  padding: 17px 5px 0 15px;
-  box-shadow: 0px 4px 12px rgba(0, 0, 0, 0.25);
-
-  .shadow {
     position: relative;
-    overflow: hidden;
-  }
+    border: 1px solid black;
+    width: 148px;
+    height: 206px;
+    background-color: white;
+    color: black;
+    border-radius: 7px;
+    font-weight: 500;
+    font-size: ${(props) => (props.shrinkFont ? '11px' : '18px')};
+    overflow-wrap: break-word;
+    line-height: 21px;
+    padding: 17px 15px 0 15px;
+    box-shadow: 0px 4px 12px rgba(0, 0, 0, 0.25);
 
-  .shadow:after {
-    content: '';
-    width: 100%;
-    height: 100%;
-    position: absolute;
-    top: 1px;
-    left: 0;
-    box-shadow: inset 0 -20px 10px -10px white;
-    pointer-events: none;
-    opacity: ${(props) => (props.hideOverflowShadow ? '0' : '1')};
-  }
+    .shadow {
+        position: relative;
+    }
 
-  .whiteCardText {
-    height: 145px !important;
-    width: 108%;
-  }
-
-  .whiteCardText p {
-    margin-bottom: 50px;
-    padding-right: 35px;
-  }
-
-  .layeredCards {
-    position: absolute;
-    bottom: 3px;
-    left: 7px;
-  }
-
-  .scrollbarThumb {
-    width: 3px;
-  }
-
-  @media (min-width: 1025px) {
-    width: 267px;
-    height: 363px;
-    font-size: ${(props) => (props.shrinkFont ? '20px' : '36px')};
-    line-height: 42px;
-    padding: 30px 10px 0 30px;
+    .shadow:after {
+        content: '';
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        top: 0;
+        left: 0;
+        box-shadow: inset 0 -10px 10px -10px white;
+        pointer-events: none;
+    }
 
     .whiteCardText {
-      height: 280px !important;
+        max-height: 150px;
+        overflow-y: auto;
     }
-  }
+
+    .whiteCardText div {
+        padding-bottom: 20px;
+    }
+
+    .layeredCards {
+        position: absolute;
+        bottom: 0px;
+        left: 7px;
+        transform: scale(0.78);
+    }
+
+    @media (min-width: 1025px) {
+        width: 267px;
+        height: 363px;
+        font-size: ${(props) => (props.shrinkFont ? '20px' : '36px')};
+        line-height: 42px;
+        padding: 30px 27px 0 27px;
+
+        .whiteCardText {
+            max-height: 280px;
+        }
+    }
 `;
 
 function WhiteCard({ children }) {
-  const [hideOverflowShadow, setHideOverflowShadow] = useState(false);
-  function handleScroll(e) {
-    return e.target.scrollTop > e.target.scrollTopMax - 50
-      ? setHideOverflowShadow(true)
-      : setHideOverflowShadow(false);
-  }
-
   return (
-    <StyledWhiteCard hideOverflowShadow={hideOverflowShadow} data-testid="white-card" shrinkFont={children.length > 75}>
+    <StyledWhiteCard shrinkFont={children.length > 75}>
       <div className="shadow">
-        <Scrollbars
-          onScroll={handleScroll}
-          data-testid="custom-scrollbar"
-          renderThumbVertical={() => (
-            <div
-              className="scrollbarThumb"
-              style={{
-                backgroundColor: 'black',
-                position: 'relative',
-                borderRadius: '15px',
-                zIndex: 1,
-              }}
-            />
-          )}
-          autoHide
-          hideTracksWhenNotNeeded
-          className="whiteCardText"
-        >
-          <p>{children}</p>
-        </Scrollbars>
+        <div className="whiteCardText">
+          <div>{children}</div>
+        </div>
       </div>
       <div className="layeredCards">
-        <LayeredCards small />
+        <LayeredCards />
       </div>
     </StyledWhiteCard>
   );
