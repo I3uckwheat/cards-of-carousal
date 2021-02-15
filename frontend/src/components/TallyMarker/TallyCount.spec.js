53e54cf44547ea9459c5be60a6809eea588c37e8
Zach Meyer <zachary.michael.meyer@gmail.com>
2021-02-14 20:19:45 -0500

    Add tests for TallyCount

---
+++ frontend/src/components/TallyMarker/TallyCount.spec.js
@@ -0,0 +1,63 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+
+import TallyCount from './TallyCount';
+
+// https://jestjs.io/docs/en/tutorial-react#snapshot-testing-with-mocks-enzyme-and-react-16
+jest.mock('./TallyGroup', () => ({ tallyCount }) => 
+  <tally-group-mock data-testid='tally-group' data-value={ tallyCount } />
+);
+
+describe('TallyCount', () => {
+  describe('score', () => {
+    it('renders a single tallygroup when score is 5', () => {
+      render(<TallyCount color="primary" score={5} />);
+      expect(screen.queryByText('5')).not.toBeInTheDocument();
+      expect(screen.queryAllByTestId('tally-group').length).toBe(1);
+    });
+
+    it('renders a single tallygroup when score is < 5', () => {
+      render(<TallyCount color="primary" score={2} />);
+      expect(screen.queryByText('2')).not.toBeInTheDocument();
+      expect(screen.queryAllByTestId('tally-group').length).toBe(1);
+    });
+
+    it('renders two tallygroups when score is > 5', () => {
+      render(<TallyCount color="primary" score={6} />);
+      expect(screen.queryByText('6')).not.toBeInTheDocument();
+      expect(screen.queryAllByTestId('tally-group').length).toBe(2);
+    });
+
+    it('renders two tallygroups when score is 10', () => {
+      render(<TallyCount color="primary" score={10} />);
+      expect(screen.queryByText('10')).not.toBeInTheDocument();
+      expect(screen.queryAllByTestId('tally-group').length).toBe(2);
+    });
+
+    it('renders two tallygroups in the correct order when score is 8', () => {
+      render(<TallyCount color="primary" score={8} />);
+
+      // Because we're querying ALL, we can expect the order of the array to match what is 
+      // rendered in the view. QueryAllByTestId reads the DOM tree from top to bottom.
+      const tallyGroup = screen.queryAllByTestId('tally-group');
+      expect(tallyGroup[0].dataset.value).toBe('5');
+      expect(tallyGroup[1].dataset.value).toBe('3');
+    });
+
+    it('doesn\'t render two tallygroups in the incorrect order when score is 8', () => {
+      render(<TallyCount color="primary" score={8} />);
+
+      // Because we're querying ALL, we can expect the order of the array to match what is 
+      // rendered in the view. QueryAllByTestId reads the DOM tree from top to bottom.
+      const tallyGroup = screen.queryAllByTestId('tally-group');
+      expect(tallyGroup[0].dataset.value).not.toBe('3');
+      expect(tallyGroup[1].dataset.value).not.toBe('5');
+    });
+
+    it('renders the score as a number when greater than 10', () => {
+      render(<TallyCount color="primary" score={11} />);
+      expect(screen.getByText('11')).toBeInTheDocument();
+    });
+  });
+});
