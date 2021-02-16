bfd700f1ebef9a58b9332ae82ad319ecea1c443a
Pedro Klepa <pedro.klepa@gmail.com>
2021-02-16 18:34:52 +0000

    Edit some comments on .spec file

--- backend/app/lib/SocketRouter/SocketRouter.spec.js
+++ backend/app/lib/SocketRouter/SocketRouter.spec.js
@@ -1,5 +1,3 @@
-// TODO: Write tests for SocketRouter class
-
 /*
 constructor()
 - expect new SocketRouter() without handler to throw Error 'Missing 404 handler'
@@ -10,7 +8,7 @@ addRoute()
 - expect socketRouter.addRoute('routeDeclaration', handler) with wrong syntax to throw Error "Invalid route declaration, check syntax"
 - expect socketRouter.addRoute('routeDeclaration', handler) with wrong HTTP method to throw Error 'Route method must be "GET", "PUT", "POST", "DELETE"' 
 - expect socketRouter.addRoute('routeDeclaration', handler) with wrong route syntax to throw Error 'Route must start from root' 
-- verify socketRouter.addRoute('routeDeclaration', handler) with correct syntax to properly add given route
+? verify socketRouter.addRoute('routeDeclaration', handler) with correct syntax to properly add given route
 
 handleRequest()
 - expect socketRouter.handleRequest(webSocket, request) with request to undefined route to call the original handler passed as a param to socketRouter constructor
