d3ffbc2e7f8164264a4f542ef3954edb5bb1f887
sher-s7 <sher.sheikh77@gmail.com>
2021-02-18 21:56:40 -0500

    Console log the parsed JSON deck data

--- backend/app/controllers/deckController.js
+++ backend/app/controllers/deckController.js
@@ -1,6 +1,13 @@
 // const MasterDeck = require('../lib/MasterDeck/MasterDeck');
+const path = require('path');
+const fs = require('fs');
 
 exports.getDeck = function (req, res) {
+  const cardData = fs.readFileSync(
+    path.join(__dirname, '../assets/cah-cards-full.json'),
+  );
+  const cardParse = JSON.parse(cardData);
+  console.log(cardParse);
   res.send('NOT IMPLEMENTED: Return all pack names');
 };
 
