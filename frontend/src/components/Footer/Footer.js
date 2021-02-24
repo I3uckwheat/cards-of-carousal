004a5c2880eb9c483a5a121a33d693c968f14c1f
Grace Schwartz <grace.schwar@gmail.com>
2021-02-23 22:23:48 -0500

    "Clean up CSS and add links in footer
    - fix accent color in App.css to use here"

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -2,25 +2,27 @@ import styled from 'styled-components';
 import React from 'react';
 
 const FooterWrapper = styled.footer`
-  margin-top: auto;
   height: 140px;
   font-size: 2rem;
   font-weight: 400;
   padding-right: 16px;
-  background-color: var(--secondary-background-color);
-  color: var(--secondary-text-color);
+  padding-bottom: 10px;
+
   display: flex;
   flex-direction: column;
-  align-items: flex-end;
   justify-content: flex-end;
-  font-weight: 400;
 
-  .credit-1 {
-    padding: 0 16px 0 16px;
+  background-color: var(--secondary-background-color);
+  color: var(--secondary-text-color);
+
+  p {
+    text-align: right;
+    position: relative;
   }
 
-  .credit-2 {
-    padding: 0 16px 16px 16px;
+  a {
+    color: var(--secondary-text-color);
+    text-decoration: none;
   }
 
   /*small size hosting screen */
@@ -31,25 +33,22 @@ const FooterWrapper = styled.footer`
 
   /*largest desktop player mode */
   @media (max-width: 785px) {
-    align-items: center;
-    justify-content: center;
     height: 120px;
     background-color: transparent;
-    color: var(--accent-text-color);
-    text-align: center;
-    font-size: 1.2rem;
-    padding-bottom: 32px;
 
-    .credit-1 {
-      line-height: 1.5rem;
-      padding: 0 320px 16px 320px;
+    p {
+      color: var(--accent-text-color);
+      text-align: center;
+      font-size: 1.2rem;
+      padding-bottom: 32px;
     }
 
-    .credit-2 {
-      line-height: 1.5rem;
-      width: 320px;
-      margin: 0 auto;
-      padding-bottom: 28px;
+    p span {
+      display: block;
+    }
+
+    a {
+      color: var(--accent-text-color);
     }
   }
 
@@ -62,11 +61,21 @@ const FooterWrapper = styled.footer`
 function Footer() {
   return (
     <FooterWrapper>
-      <p className="credit-1">
-        Card content thanks to: https://cardsagainsthumanity.com
+      <p>
+        Card content thanks to:{' '}
+        <span>
+          <a href="https://cardsagainsthumanity.com">
+            https://cardsagainsthumanity.com
+          </a>
+        </span>
       </p>
-      <p className="credit-2">
-        Made by Odin students, with love: https://www.theodinproject.com
+      <p>
+        Made by Odin students, with love:{' '}
+        <span>
+          <a href="https://www.theodinproject.com">
+            https://www.theodinproject.com
+          </a>
+        </span>
       </p>
     </FooterWrapper>
   );
