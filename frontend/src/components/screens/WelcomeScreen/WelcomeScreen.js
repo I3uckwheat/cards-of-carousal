4f379bd1d77af755bce78104c6fdd688ba538f0d
Grace Schwartz <grace.schwar@gmail.com>
2021-02-24 20:21:51 -0500

    Update Components to look good on 4k screens

--- frontend/src/components/screens/WelcomeScreen/WelcomeScreen.js
+++ frontend/src/components/screens/WelcomeScreen/WelcomeScreen.js
@@ -17,22 +17,23 @@ const WelcomeScreenWrapper = styled.div`
   /*Header*/
   .header-container {
     display: flex;
-    height: 190px;
+    height: 400px;
     flex-direction: column;
     justify-content: flex-end;
   }
 
   .welcome-to {
+    font-size: 3.5rem;
     margin-top: 0px;
-    line-height: 2rem;
-    margin-bottom: -0.14px;
+    line-height: 5rem;
+    margin-bottom: -14px;
   }
 
   .CoC {
-    font-size: 6rem;
-    margin-bottom: -16px;
+    font-size: 9rem;
+    margin-bottom: -5px;
     vertical-align: bottom;
-    line-height: 6rem;
+    line-height: 6.5rem;
   }
 
   /*Main Body*/
@@ -47,15 +48,15 @@ const WelcomeScreenWrapper = styled.div`
   .definition-container {
     display: flex;
     flex-direction: column;
-    font-size: 3.2rem;
+    font-size: 9rem;
     font-weight: 700;
     transform: rotate(-5deg);
-    margin-bottom: 144px;
-    line-height: 3.3rem;
+    margin-bottom: 280px;
+    line-height: 6rem;
   }
 
   .definition {
-    font-size: 2.75rem;
+    font-size: 7rem;
     font-weight: 400;
   }
 
@@ -69,9 +70,9 @@ const WelcomeScreenWrapper = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
-    width: 256px;
-    height: 48px;
-    font-size: 2.5rem;
+    width: 700px;
+    height: 180px;
+    font-size: 6rem;
     font-weight: 900;
   }
 
@@ -83,29 +84,75 @@ const WelcomeScreenWrapper = styled.div`
     color: var(--secondary-text-color);
     border-radius: 50%;
     padding: 16px;
-    width: 72px;
-    height: 72px;
+    width: 180px;
+    height: 180px;
     margin-left: 93px;
     margin-right: 93px;
     font-size: 2.25rem;
     font-weight: 700;
   }
 
-  /*Footer*/
-  .footer-container {
-    display: flex;
-    flex-direction: column;
-    align-items: flex-end;
-    justify-content: flex-end;
-    margin-top: auto;
-    height: 140px;
-    background-color: var(--secondary-background-color);
-    color: var(--secondary-text-color);
-    font-size: 2rem;
-    font-weight: 400;
-    padding-right: 20px;
-  }
+  @media (max-width: 2000px) {
+    /*Header*/
+    .header-container {
+      display: flex;
+      height: 190px;
+      flex-direction: column;
+      justify-content: flex-end;
+    }
 
+    .welcome-to {
+      font-size: 2rem;
+      margin-top: 0px;
+      line-height: 2rem;
+      margin-bottom: -16px;
+    }
+
+    .CoC {
+      font-size: 6rem;
+      margin-bottom: -16px;
+      line-height: 6rem;
+    }
+
+    .definition-container {
+      font-size: 3.2rem;
+      font-weight: 700;
+      transform: rotate(-5deg);
+      margin-bottom: 144px;
+      line-height: 3.3rem;
+    }
+
+    .definition {
+      font-size: 2.75rem;
+      font-weight: 400;
+    }
+
+    .join-btn {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 256px;
+      height: 48px;
+      font-size: 2.5rem;
+      font-weight: 900;
+    }
+
+    .OR {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      background-color: var(--secondary-background-color);
+      color: var(--secondary-text-color);
+      border-radius: 50%;
+      padding: 16px;
+      width: 72px;
+      height: 72px;
+      margin-left: 93px;
+      margin-right: 93px;
+      font-size: 2.25rem;
+      font-weight: 700;
+    }
+  }
   /*small size hosting screen */
   @media (max-width: 970px) {
     .definition-container {
@@ -149,7 +196,6 @@ const WelcomeScreenWrapper = styled.div`
     }
   }
 
-  /* Keeps footer content formatted correctly as player only screen shrinks on desktop*/
   @media (max-width: 645px) {
     .welcome-to {
       display: none;
