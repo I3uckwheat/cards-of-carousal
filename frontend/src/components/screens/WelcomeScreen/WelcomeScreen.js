b7c499f9c6bac8fc1b59da99830f6a9c8a0376f3
Grace Schwartz <grace.schwar@gmail.com>
2021-02-25 23:11:41 -0500

    WIP CSS responsivness for all screen sizes

--- frontend/src/components/screens/WelcomeScreen/WelcomeScreen.js
+++ frontend/src/components/screens/WelcomeScreen/WelcomeScreen.js
@@ -13,11 +13,282 @@ const WelcomeScreenWrapper = styled.div`
   bottom: 0;
   left: 0;
   right: 0;
-
+  height: 100%;
   background-color: var(--primary-background-color);
 
-  /*Header*/
-  .header-container {
+  .header {
+    height: 190px;
+  }
+
+  .header-text {
+    position: relative;
+    bottom: -122px;
+  }
+
+  .welcome-to {
+    font-size: 2rem;
+    line-height: 0.2rem;
+    font-weight: 500;
+
+    margin-top: 0px;
+    margin-bottom: -14px;
+  }
+
+  .CoC {
+    font-size: 5rem;
+    line-height: 6.8rem;
+    font-weight: 700;
+
+    margin-bottom: -5px;
+    vertical-align: bottom;
+  }
+
+  main {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin: auto;
+  }
+
+  .definition-container {
+    display: flex;
+    flex-direction: column;
+
+    font-size: 3rem;
+    font-weight: 700;
+    line-height: 2rem;
+
+    transform: rotate(-5deg);
+    margin-bottom: 90px;
+  }
+
+  .definition {
+    font-size: 2rem;
+    font-weight: 400;
+  }
+
+  .definition-container p {
+    display: inline-block;
+  }
+
+  .button-container {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
+
+  .btn {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    font-size: 6rem;
+    font-weight: 900;
+
+    width: 256px;
+    height: 48px;
+
+    font-size: 2.5rem;
+  }
+
+  .OR {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    background-color: var(--secondary-background-color);
+    color: var(--secondary-text-color);
+
+    border-radius: 50%;
+    padding: 16px;
+
+    font-size: 4rem;
+    font-weight: 700;
+
+    width: 72px;
+    height: 72px;
+    margin-left: 93px;
+    margin-right: 93px;
+
+    font-size: 2.25rem;
+  }
+
+  /*if less than 890px do this.... */
+  @media (max-width: 820px) {
+    .header-text {
+      bottom: -134px;
+    }
+
+    .welcome-to {
+      font-size: 1.5rem;
+      line-height: 0.2rem;
+    }
+
+    .CoC {
+      font-size: 4rem;
+      line-height: 6rem;
+    }
+
+    .definition-container {
+      font-size: 2.2rem;
+      line-height: 1.8rem;
+
+      transform: rotate(-5deg);
+      margin-bottom: 90px;
+    }
+
+    .definition {
+      font-size: 1.7rem;
+    }
+
+    .btn {
+      font-size: 6rem;
+      font-weight: 900;
+
+      width: 200px;
+      height: 48px;
+
+      font-size: 2.5rem;
+    }
+
+    .OR {
+      border-radius: 50%;
+      padding: 16px;
+
+      font-size: 4rem;
+
+      width: 60px;
+      height: 60px;
+      margin-left: 70px;
+      margin-right: 70px;
+
+      font-size: 2rem;
+    }
+  }
+
+  /*if less than 700px do this.... */
+  @media (max-width: 700px) {
+    .header {
+      height: 190px;
+    }
+
+    .header-text {
+      bottom: -133px;
+    }
+    .welcome-to {
+      font-size: 1.5rem;
+      line-height: 0.2rem;
+
+      margin-top: 0px;
+      margin-bottom: -14px;
+    }
+
+    .CoC {
+      font-size: 4rem;
+      line-height: 6rem;
+      margin-bottom: -15px;
+    }
+
+    .definition-container {
+      display: none;
+      margin-bottom: 48px;
+    }
+
+    .btn {
+      width: 224px;
+    }
+
+    .host-btn {
+      display: none;
+    }
+
+    .OR {
+      display: none;
+    }
+  }
+
+  /*if less than 655px do this.... */
+  @media (max-width: 655px) {
+    .header-text {
+      bottom: -86px;
+    }
+
+    .welcome-to {
+      display: none;
+    }
+
+    .CoC {
+      line-height: 3.5rem;
+    }
+  }
+
+  /*if less than 320px do this.... */
+  @media (max-width: 320px) {
+    .header {
+      height: 130px;
+    }
+
+    .header-text {
+      bottom: -30px;
+    }
+
+    .CoC {
+      font-size: 3rem;
+      line-height: 3.5rem;
+
+      margin-bottom: 12px;
+    }
+  }
+
+  /*if greater than 2450px do this.... */
+  @media (min-width: 2450px) {
+    .header {
+      height: 400px;
+    }
+
+    .header-text {
+      bottom: -260px;
+    }
+
+    .welcome-to {
+      font-size: 3rem;
+      line-height: 3.8rem;
+    }
+
+    .CoC {
+      font-size: 8rem;
+      line-height: 6.5rem;
+    }
+
+    .definition-container {
+      font-size: 6.5rem;
+      line-height: 5.5rem;
+      margin-bottom: 300px;
+    }
+
+    .definition {
+      font-size: 6rem;
+    }
+
+    .OR {
+      font-size: 4.5rem;
+
+      width: 150px;
+      height: 150px;
+      margin-left: 120px;
+      margin-right: 120px;
+    }
+
+    .btn {
+      width: 600px;
+      height: 150px;
+      border: solid black 4px;
+      font-size: 5rem;
+    }
+  }
+
+  /* .header-container {
     display: flex;
     flex-direction: column;
     justify-content: flex-end;
@@ -40,7 +311,6 @@ const WelcomeScreenWrapper = styled.div`
     vertical-align: bottom;
   }
 
-  /*Main Body*/
   main {
     display: flex;
     flex-direction: column;
@@ -104,7 +374,6 @@ const WelcomeScreenWrapper = styled.div`
   }
 
   @media (max-width: 2000px) {
-    /*Header*/
     .header-container {
       height: 190px;
     }
@@ -148,7 +417,7 @@ const WelcomeScreenWrapper = styled.div`
       font-size: 2.25rem;
     }
   }
-  /*small size hosting screen */
+
   @media (max-width: 970px) {
     .definition-container {
       margin-bottom: 80px;
@@ -161,7 +430,6 @@ const WelcomeScreenWrapper = styled.div`
     }
   }
 
-  /*largest desktop player mode */
   @media (max-width: 785px) {
     .header-container {
       height: 180px;
@@ -197,7 +465,6 @@ const WelcomeScreenWrapper = styled.div`
     }
   }
 
-  /*iphone 5*/
   @media (max-width: 320px) {
     .header-container {
       height: 130px;
@@ -210,30 +477,35 @@ const WelcomeScreenWrapper = styled.div`
       margin-bottom: 12px;
       vertical-align: bottom;
     }
-  }
+  } */
 `;
 
 function WelcomeScreen() {
   return (
     <WelcomeScreenWrapper className="primary-background">
-      <Header className="header-container">
-        <h1 className="welcome-to">WELCOME TO</h1>
-        <h2 className="CoC">CARDS OF CAROUSAL</h2>
+      <Header className="header">
+        <div className="header-text">
+          <p className="welcome-to">WELCOME TO</p>
+          <h1 className="CoC">CARDS OF CAROUSAL</h1>
+        </div>
       </Header>
       <main>
         <div className="definition-container">
-          <div className="word">
+          <p className="word">
             CAROUSAL <span className="definition">(n)</span>
-          </div>
-          <div className="definition">
+          </p>
+          <p className="definition">
             a wild, drunken party or celebration : a drunken revel
-          </div>
+          </p>
         </div>
+
         <div className="button-container">
           <Button type="button" isActive className="btn">
-            <p> JOIN</p>
+            <p>JOIN</p>
           </Button>
+
           <div className="OR">OR</div>
+
           <Button type="button" isActive className="btn host-btn">
             <p>HOST</p>
           </Button>
