0a85997d08b77734bb504a83b0f57e2a6e597488
Matt B <matthew.bowler123@gmail.com>
2021-02-27 14:26:39 -0500

    Prettier changes

--- backend/app/lib/Lobby/Lobby.spec.js
+++ backend/app/lib/Lobby/Lobby.spec.js
@@ -8,8 +8,8 @@ let createSocket = () => {
     on: jest.fn(),
     send: jest.fn(),
     close: jest.fn(),
-  }
-}
+  };
+};
 
 describe('Lobby', () => {
   let lobby;
@@ -34,11 +34,11 @@ describe('Lobby', () => {
       expect(hostSocket.on).toBeCalledWith('close', expect.any(Function));
       expect(hostSocket.send).toBeCalledWith(JSON.stringify(messageObject));
     });
-  })
+  });
 
   describe('has multiple players', () => {
     let playerSocket = createSocket();
-    let playerSocketTwo = createSocket()
+    let playerSocketTwo = createSocket();
     let messageObject = {
       event: '',
       payload: {},
@@ -48,28 +48,30 @@ describe('Lobby', () => {
     describe('addPlayer', () => {
       it('can add a player and sends message through the socket', () => {
         messageObject.event = 'player-connect';
-        messageObject.payload = { playerId: playerSocket.id }
+        messageObject.payload = { playerId: playerSocket.id };
         lobby.addPlayer(playerSocket);
         expect(playerSocket.on).toBeCalledWith('message', expect.any(Function));
         expect(playerSocket.on).toBeCalledWith('close', expect.any(Function));
         expect(hostSocket.send).toBeCalledWith(JSON.stringify(messageObject));
       });
-    })
+    });
 
     describe('closeLobby', () => {
       it('can close a lobby and send message through the socket for each player', () => {
         messageObject.event = 'lobby-closed';
         messageObject.payload = {};
-  
+
         lobby.addPlayer(playerSocket);
         lobby.addPlayer(playerSocketTwo);
         lobby.closeLobby();
         expect(playerSocket.send).toBeCalledWith(JSON.stringify(messageObject));
         expect(playerSocket.close).toBeCalledWith(1000, messageObject.event);
-        expect(playerSocketTwo.send).toBeCalledWith(JSON.stringify(messageObject));
+        expect(playerSocketTwo.send).toBeCalledWith(
+          JSON.stringify(messageObject),
+        );
         expect(playerSocketTwo.close).toBeCalledWith(1000, messageObject.event);
         expect(onCloseCallBack).toBeCalledWith(lobby.id);
       });
-    })
-  })
+    });
+  });
 });
