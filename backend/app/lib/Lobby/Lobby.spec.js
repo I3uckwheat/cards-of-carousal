ac6f75d45af694429a838f206222d2d62357d1f2
Matt B <matthew.bowler123@gmail.com>
2021-02-27 14:20:00 -0500

    Adding socket object creator, adding more indepth describe blocks

--- backend/app/lib/Lobby/Lobby.spec.js
+++ backend/app/lib/Lobby/Lobby.spec.js
@@ -2,71 +2,74 @@ const Lobby = require('./Lobby');
 const { customAlphabet } = require('nanoid');
 const customNanoid = customAlphabet('ABCDGHJKMNPRSTUVWXYZ', 4);
 
+let createSocket = () => {
+  return {
+    id: customNanoid(),
+    on: jest.fn(),
+    send: jest.fn(),
+    close: jest.fn(),
+  }
+}
+
 describe('Lobby', () => {
   let lobby;
   let hostSocket;
   let onCloseCallBack;
-  let playerSocket;
   beforeEach(() => {
     jest.clearAllMocks();
-    hostSocket = {
-      on: jest.fn(),
-      send: jest.fn(),
-      close: jest.fn(),
-    };
+    hostSocket = createSocket();
     onCloseCallBack = jest.fn();
     lobby = new Lobby(hostSocket, onCloseCallBack);
-    playerSocket = {
-      id: customNanoid(),
-      on: jest.fn(),
-      send: jest.fn(),
-      close: jest.fn(),
-    };
   });
 
-  it('gives the lobby a unique ID and sends message through the socket', () => {
-    let messageObject = {
-      event: 'create-lobby',
-      payload: { id: lobby.id },
-      sender: 'server',
-    };
-    expect(lobby.id).toBeTruthy();
-    expect(hostSocket.on).toBeCalledWith('message', expect.any(Function));
-    expect(hostSocket.on).toBeCalledWith('close', expect.any(Function));
-    expect(hostSocket.send).toBeCalledWith(JSON.stringify(messageObject));
-  });
+  describe('constructor', () => {
+    it('gives the lobby a unique ID and sends message through the socket', () => {
+      let messageObject = {
+        event: 'create-lobby',
+        payload: { id: lobby.id },
+        sender: 'server',
+      };
+      expect(lobby.id).toBeTruthy();
+      expect(hostSocket.on).toBeCalledWith('message', expect.any(Function));
+      expect(hostSocket.on).toBeCalledWith('close', expect.any(Function));
+      expect(hostSocket.send).toBeCalledWith(JSON.stringify(messageObject));
+    });
+  })
 
-  it('can add a player and sends message through the socket', () => {
+  describe('has multiple players', () => {
+    let playerSocket = createSocket();
+    let playerSocketTwo = createSocket()
     let messageObject = {
-      event: 'player-connect',
-      payload: { playerId: playerSocket.id },
-      sender: 'server',
-    };
-    lobby.addPlayer(playerSocket);
-    expect(playerSocket.on).toBeCalledWith('message', expect.any(Function));
-    expect(playerSocket.on).toBeCalledWith('close', expect.any(Function));
-    expect(hostSocket.send).toBeCalledWith(JSON.stringify(messageObject));
-  });
-
-  it('can close a lobby and send message through the socket for each player', () => {
-    let messageObject = {
-      event: 'lobby-closed',
+      event: '',
       payload: {},
       sender: 'server',
     };
-    let playerSocketTwo = {
-      id: customNanoid(),
-      on: jest.fn(),
-      send: jest.fn(),
-      close: jest.fn(),
-    };
-    lobby.addPlayer(playerSocket);
-    lobby.addPlayer(playerSocketTwo);
-    lobby.closeLobby();
-    expect(playerSocket.send).toBeCalledWith(JSON.stringify(messageObject));
-    expect(playerSocket.close).toBeCalledWith(1000, messageObject.event);
-    expect(playerSocketTwo.send).toBeCalledWith(JSON.stringify(messageObject));
-    expect(playerSocketTwo.close).toBeCalledWith(1000, messageObject.event);
-    expect(onCloseCallBack).toBeCalledWith(lobby.id);
-  });
+
+    describe('addPlayer', () => {
+      it('can add a player and sends message through the socket', () => {
+        messageObject.event = 'player-connect';
+        messageObject.payload = { playerId: playerSocket.id }
+        lobby.addPlayer(playerSocket);
+        expect(playerSocket.on).toBeCalledWith('message', expect.any(Function));
+        expect(playerSocket.on).toBeCalledWith('close', expect.any(Function));
+        expect(hostSocket.send).toBeCalledWith(JSON.stringify(messageObject));
+      });
+    })
+
+    describe('closeLobby', () => {
+      it('can close a lobby and send message through the socket for each player', () => {
+        messageObject.event = 'lobby-closed';
+        messageObject.payload = {};
+  
+        lobby.addPlayer(playerSocket);
+        lobby.addPlayer(playerSocketTwo);
+        lobby.closeLobby();
+        expect(playerSocket.send).toBeCalledWith(JSON.stringify(messageObject));
+        expect(playerSocket.close).toBeCalledWith(1000, messageObject.event);
+        expect(playerSocketTwo.send).toBeCalledWith(JSON.stringify(messageObject));
+        expect(playerSocketTwo.close).toBeCalledWith(1000, messageObject.event);
+        expect(onCloseCallBack).toBeCalledWith(lobby.id);
+      });
+    })
+  })
 });
