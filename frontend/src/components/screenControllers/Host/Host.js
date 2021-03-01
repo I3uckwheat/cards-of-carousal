82b8a418d1f8b2afc595f250a3b4211458335215
aplyd <austin@ftacnik.com>
2021-03-01 14:25:50 -0800

    create file structure and needed placeholder components

---
+++ frontend/src/components/screenControllers/Host/Host.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import HostLayout from '../../layouts/HostLayout';
+
+const propTypes = {};
+
+export default function Host() {
+  return <HostLayout />;
+}
+
+Host.propTypes = propTypes;
---
+++ frontend/src/components/screenControllers/Player/Player.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PlayerJoinScreen from '../../screens/PlayerJoinScreen/PlayerJoinScreen';
+
+const propTypes = {};
+
+export default function Player() {
+  return <PlayerJoinScreen />;
+}
+
+Player.propTypes = propTypes;
