cb42d15279dca33102da5b8a85e409a6e5edc470
Sully <a.m.sullivan.github@gmail.com>
2021-03-02 00:46:26 -0500

    Revise Host Settings Menu static styling to match figma
    More specifically refactor the previous example settings menu to use
    the Header component, use the right function name, have styled buttons
    in the white space, and simplify the component to remove the former
    'white-box' div.

--- frontend/src/components/HostSettingsMenu/HostSettingsMenu.js
+++ frontend/src/components/HostSettingsMenu/HostSettingsMenu.js
@@ -1,38 +1,61 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const SettingsModal = styled.div`
+import Header from '../Header/Header';
+
+const SettingsMenu = styled.div`
   position: absolute;
   top: 0;
   right: 0;
   bottom: 0;
-  height: 100%;
-  width: 25%;
-  background-color: var(--secondary-background-color);
+  width: 30%;
+  background-color: var(--primary-background-color);
   display: flex;
   flex-direction: column;
 
-  .settings-text {
-    font-size: 3rem;
-    line-height: 3rem;
-    margin-bottom: -11px;
-    color: var(--secondary-text-color);
+  .host-settings-header {
     display: flex;
-    flex-direction: column-reverse;
-    flex: 1;
+    align-items: flex-end;
+    height: 130px;
   }
 
-  .white-box {
-    background-color: var(--primary-background-color);
-    flex: 7;
+  .host-settings-header h3 {
+    font-size: 3.5rem;
+    line-height: 2.1rem;
+    color: var(--secondary-text-color);
+  }
+
+  .settings-item {
+    color: var(--primary-text-color);
+    background-color: transparent;
+    border: none;
+
+    text-align: left;
+    font-size: 1.5rem;
+    font-weight: bold;
+  }
+
+  .option-button {
+    padding: 30px 0 20px 25px;
+  }
+
+  .option-list {
+    padding: 20px 25px;
   }
 `;
 
-export default function SettingsMenuExample() {
+export default function HostSettingsMenu() {
   return (
-    <SettingsModal>
-      <h3 className="settings-text">SETTINGS</h3>
-      <div className="white-box" />
-    </SettingsModal>
+    <SettingsMenu>
+      <Header className="host-settings-header">
+        <h3>SETTINGS</h3>
+      </Header>
+      <button type="button" className="settings-item option-button">
+        SKIP UNUSED SELECTIONS
+      </button>
+      <button type="button" className="settings-item option-list">
+        KICK PLAYER
+      </button>
+    </SettingsMenu>
   );
 }
