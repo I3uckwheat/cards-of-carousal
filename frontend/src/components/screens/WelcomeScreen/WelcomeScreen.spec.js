72060e19ee91297e71103da2a7777f022803141b
aplyd <austin@ftacnik.com>
2021-03-03 10:44:28 -0800

    add tests for callbacks added to join and host buttons

--- frontend/src/components/screens/WelcomeScreen/WelcomeScreen.spec.js
+++ frontend/src/components/screens/WelcomeScreen/WelcomeScreen.spec.js
@@ -1,15 +1,50 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { fireEvent, render } from '@testing-library/react';
+
 import WelcomeScreen from './WelcomeScreen';
 
 describe('WelcomeScreen', () => {
-  it('renders correctly', () => {
-    const tree = renderer
-      .create(
-        <WelcomeScreen handleJoinClick={() => ''} handleHostClick={() => ''} />,
-      )
-      .toJSON();
+  describe('render', () => {
+    it('renders correctly', () => {
+      const tree = renderer
+        .create(
+          <WelcomeScreen
+            handleJoinClick={() => ''}
+            handleHostClick={() => ''}
+          />,
+        )
+        .toJSON();
 
-    expect(tree).toMatchSnapshot();
+      expect(tree).toMatchSnapshot();
+    });
+  });
+
+  describe('events', () => {
+    it('fires callback when "join" button is clicked', () => {
+      const mockJoinBtnCallback = jest.fn();
+      const { getByText } = render(
+        <WelcomeScreen
+          handleJoinClick={mockJoinBtnCallback}
+          handleHostClick={() => ''}
+        />,
+      );
+
+      fireEvent.click(getByText('JOIN'));
+      expect(mockJoinBtnCallback).toHaveBeenCalledTimes(1);
+    });
+
+    it('fires callback when "host" button is clicked', () => {
+      const mockHostBtnCallback = jest.fn();
+      const { getByText } = render(
+        <WelcomeScreen
+          handleJoinClick={() => ''}
+          handleHostClick={mockHostBtnCallback}
+        />,
+      );
+
+      fireEvent.click(getByText('HOST'));
+      expect(mockHostBtnCallback).toHaveBeenCalledTimes(1);
+    });
   });
 });
