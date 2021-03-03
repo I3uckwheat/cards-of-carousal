b37b02acbac4793ee1902cf8ad581d39ef03dbc7
Pedro Klepa <pedro.klepa@gmail.com>
2021-03-03 19:30:14 +0000

    Subscribe to events coming from socketInstance

--- frontend/src/contexts/HostContext/HostContext.js
+++ frontend/src/contexts/HostContext/HostContext.js
@@ -1,11 +1,14 @@
-import React, { createContext, useReducer } from 'react';
+import React, { createContext, useEffect, useReducer } from 'react';
 import PropTypes from 'prop-types';
 
 import reducer from './HostReducer';
+import socketInstance from '../../socket/socket';
 
 const propTypes = {
   children: PropTypes.node.isRequired,
 };
+
+const { emitter } = socketInstance;
 
 export const HostContext = createContext();
 
@@ -18,6 +21,32 @@ const initialState = {
 
 function HostProvider({ children }) {
   const [state, dispatch] = useReducer(reducer, initialState);
+
+  function handleMessage({ event, payload }) {
+    // Development only: log messages to console
+    if (process.env.NODE_ENV === 'development') {
+      // eslint-disable-next-line no-console
+      console.log('Handling message: ', { event, payload });
+    }
+
+    switch (event) {
+      case 'create-lobby':
+        return dispatch({ type: 'CREATE_LOBBY', payload });
+      case 'player-join':
+        return dispatch({ type: 'PLAYER_JOIN', payload });
+      case 'player-disconnect':
+        return dispatch({ type: 'PLAYER_DISCONNECT', payload });
+      default:
+        return undefined;
+    }
+  }
+
+  useEffect(() => {
+    emitter.on('message', handleMessage);
+    return () => {
+      emitter.off('message', handleMessage);
+    };
+  }, []);
 
   return (
     <HostContext.Provider value={{ state, dispatch }}>
