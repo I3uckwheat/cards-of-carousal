a226bb82dc07b1d47a78b35602c908a600ee3b99
aplyd <austin@ftacnik.com>
2021-03-03 10:49:11 -0800

    add local state and use WelcomeScreen callbacks to take user to the correct screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './reset.css';
 import './App.css';
 
 import WelcomeScreen from './components/screens/WelcomeScreen/WelcomeScreen';
+import Host from './components/screenControllers/Host/Host';
+import Player from './components/screenControllers/Player/Player';
 
 function App() {
+  const [showHostScreenController, setShowHostScreenController] = useState(
+    false,
+  );
+  const [showPlayerScreenController, setShowPlayerScreenController] = useState(
+    false,
+  );
+
+  if (showHostScreenController) {
+    return (
+      <Host left={<p>left</p>} right={<p>right</p>} modal={<p>modal</p>} />
+    );
+  }
+
+  if (showPlayerScreenController) {
+    return <Player />;
+  }
+
   return (
-    <WelcomeScreen handleJoinClick={() => ''} handleHostClick={() => ''} />
+    <WelcomeScreen
+      handleJoinClick={() => setShowPlayerScreenController(true)}
+      handleHostClick={() => setShowHostScreenController(true)}
+    />
   );
 }
 
