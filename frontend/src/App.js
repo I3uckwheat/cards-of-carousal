406b103773a8a5c7b8d3c1f44d2a9cb83a0d882b
Zach Meyer <zachary.michael.meyer@gmail.com>
2021-03-02 22:37:40 -0500

    Add div component with header and form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -22,6 +22,7 @@ import SingleBlackCardExample from './temp/SingleBlackCardExample.js';
 import PlayerMessageScreen from './components/screens/PlayerMessageScreen/PlayerMessageScreen';
 import HostLayout from './components/layouts/HostLayout';
 import SettingsMenuExample from './temp/SettingsMenuExample';
+import GameSettings from './components/GameSettings/GameSettings';
 
 function App() {
   const [showSocketTest, setShowSocketTest] = useState(false);
@@ -37,7 +38,8 @@ function App() {
   const [showWelcomeScreen, setShowWelcomeScreen] = useState(false);
   const [showHostLayout, setShowHostLayout] = useState(false);
   const [showCardWrapper, setShowCardWrapper] = useState(false);
-  const [showPlayerJoin, setShowPlayerJoin] = useState(true);
+  const [showPlayerJoin, setShowPlayerJoin] = useState(false);
+  const [showGameSettings, setShowGameSettings] = useState(false);
 
   return (
     <div className="App primary-background">
@@ -142,6 +144,12 @@ function App() {
         >
           Show Player Join screen
         </Button>
+        <Button
+          type="button"
+          onClick={() => setShowGameSettings(!showGameSettings)}
+        >
+          Show Game Settings
+        </Button>
       </div>
 
       {showHeader && (
@@ -244,6 +252,7 @@ function App() {
       )}
       {showCardWrapper && <CardWrapperExample />}
       {showPlayerJoin && <PlayerJoinScreen />}
+      {showGameSettings && <GameSettings />}
     </div>
   );
 }
--- frontend/src/components/GameSettings/GameSettings.js
+++ frontend/src/components/GameSettings/GameSettings.js
@@ -1,5 +1,6 @@
-import styled from 'styled-components';
+import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
 const propTypes = {
   children: PropTypes.node.isRequired,
@@ -13,4 +14,44 @@ const Header = styled.header`
 
 Header.propTypes = propTypes;
 
-export default Header;
+const StyledGameSettings = styled.div`
+  position: relative;
+  display: flex;
+  height: 372px;
+  width: 100%;
+  background-color: var(--secondary-background-color);
+  color: var(--secondary-text-color);
+`;
+
+const StyledH1 = styled.h1`
+  margin-left: auto;
+  font-size: 4rem;
+  line-height: 4rem;
+  margin-top: -0.5rem;
+`;
+
+
+function GameSettings() {
+  return (
+    <StyledGameSettings>
+      <StyledH1>
+        GAME SETTINGS
+      </StyledH1>
+      <br />
+      <form>
+        <label htmlFor="mplayers" id="mplayers">
+          MAX PLAYERS
+          <input type="number" id="mplayers" name="mplayers" value="5" /><br/>
+          </label>
+          <label htmlFor="wscore" id="wscore">
+          WINNING SCORE
+          <input type="number" id="wscore" name="wscore" value="5"/>
+          </label>
+          <p>SELECT CARD PACKS</p>
+      </form>
+    </StyledGameSettings>
+    
+  )
+}
+
+export default GameSettings;
