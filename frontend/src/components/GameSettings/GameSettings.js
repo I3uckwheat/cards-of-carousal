28d4db7d81c35ecd5ca29c7a64a628e081d4b0f7
Zach Meyer <zachary.michael.meyer@gmail.com>
2021-03-06 15:01:20 -0500

    Add API request and CSS refactor

--- frontend/src/components/GameSettings/GameSettings.js
+++ frontend/src/components/GameSettings/GameSettings.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 // import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
@@ -9,26 +9,29 @@ const StyledGameSettings = styled.div`
   height: 100%;
   background-color: var(--secondary-background-color);
   color: var(--secondary-text-color);
+
+  h1 {
+    margin-left: auto;
+    font-size: 4rem;
+    line-height: 4rem;
+    margin-top: -0.5rem;
+    justify-content: center;
+    position: absolute;
+    right: 0;
+    top: 0;
+  }
 `;
 
-const StyledH1 = styled.h1`
-  margin-left: auto;
-  font-size: 4rem;
-  line-height: 4rem;
-  margin-top: -0.5rem;
-  justify-content: center;
-  position: absolute;
-  right: 0;
-  top: 0;
-`;
-
-const StyledForm = styled.div`
+const StyledForm = styled.form`
   display: flex;
   flex-direction: column;
   justify-content: center;
   margin: 24px auto 24px 60px;
 
   .label {
+    display: flex;
+    width: 400px;
+    justify-content: space-between;
     font-weight: bold;
   }
 
@@ -67,28 +70,35 @@ const StyledForm = styled.div`
   }
 `;
 
-function createDummyPacks() {
-  const packs = {};
-  for (let i = 0; i < 30; i += 1) {
-    packs[i] = { name: `Card Pack ${i}` };
-  }
-  return packs;
+async function getPackNames() {
+  const response = await fetch('http://localhost:4000/deck');
+  const data = await response.json();
+  return data;
 }
 
+// function createDummyPacks() {
+//   const packs = {};
+//   for (let i = 0; i < 30; i += 1) {
+//     packs[i] = { name: `Card Pack ${i}` };
+//   }
+//   return packs;
+// }
+
 function GameSettings() {
-  const [cardPacks] = useState(createDummyPacks());
-  // eslint-disable-next-line no-console
-  // console.log(Object.values(cardPacks));
+  const [cardPacks, setCardPacks] = useState([]);
+
+  useEffect(async () => {
+    const packNames = await getPackNames();
+    setCardPacks(packNames);
+  }, []);
+
   return (
     <StyledGameSettings>
-      <StyledH1>GAME SETTINGS</StyledH1>
-      <form>
-        <StyledForm>
-          <div className="f input-wrapper">
-            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-            <label htmlFor="mplayers" id="mplayers" className="label">
-              MAX PLAYERS
-            </label>
+      <h1>GAME SETTINGS</h1>
+      <StyledForm>
+        <div className="f input-wrapper">
+          <label htmlFor="mplayers" id="mplayers" className="label">
+            MAX PLAYERS
             <input
               type="number"
               id="mplayers"
@@ -96,12 +106,12 @@ function GameSettings() {
               value="5"
               className="input"
             />
-          </div>
-          <div className="f input-wrapper">
-            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-            <label htmlFor="wscore" id="wscore" className="label">
-              WINNING SCORE
-            </label>
+          </label>
+        </div>
+
+        <div className="f input-wrapper">
+          <label htmlFor="wscore" id="wscore" className="label">
+            WINNING SCORE
             <input
               type="number"
               id="wscore"
@@ -109,27 +119,23 @@ function GameSettings() {
               value="5"
               className="input"
             />
-          </div>
-          <div className="select-wrapper f">
-            <label htmlFor="packs" className="select-card">
-              SELECT CARD PACKS
-              <div className="card-packs f">
-                {Object.values(cardPacks).map((pack) => (
-                  <div className="f pack">
-                    <label htmlFor="pack">{pack.name}</label>
-                    <input
-                      value={pack.name}
-                      id="pack"
-                      type="checkbox"
-                      name="pack"
-                    />
-                  </div>
-                ))}
-              </div>
-            </label>
-          </div>
-        </StyledForm>
-      </form>
+          </label>
+        </div>
+
+        <div className="select-wrapper f">
+          <label htmlFor="packs" className="select-card">
+            SELECT CARD PACKS
+            <div className="card-packs f">
+              {cardPacks.map((name) => (
+                <div className="f pack">
+                  <label htmlFor={name}>{name}</label>
+                  <input value={name} id={name} type="checkbox" name="pack" />
+                </div>
+              ))}
+            </div>
+          </label>
+        </div>
+      </StyledForm>
     </StyledGameSettings>
   );
 }
