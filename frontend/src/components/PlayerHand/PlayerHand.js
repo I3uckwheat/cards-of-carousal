0b12f1e73577b4c82d24d7c85c16774e0342cd59
Grace Schwartz <grace.schwar@gmail.com>
2021-03-10 12:33:50 -0500

    Remove comments and console.logs

--- frontend/src/components/PlayerHand/PlayerHand.js
+++ frontend/src/components/PlayerHand/PlayerHand.js
@@ -1,4 +1,5 @@
 /* to test this component, import the 'PlayerHandExample' in 'Temp' into your App.js */
+
 import React from 'react';
 import PropTypes from 'prop-types';
 import WhiteCard from '../Cards/WhiteCard';
@@ -12,17 +13,13 @@ const propType = {
 
 export default function PlayerHand({ cards, selected, onSelect }) {
   const handleClick = (index) => {
-    const newArray = [...selected]; // could also do selected.splice() to make a new copy idk if theres a preference
+    const newArray = [...selected];
 
     if (selected.includes(index)) {
       const indexOfSelected = selected.indexOf(index); // finds index in selected[] that  that contains the clicked card
       newArray.splice(indexOfSelected, 1);
-
-      console.log('IN selected, position:', indexOfSelected);
     } else {
       newArray.push(index);
-
-      console.log('not in selected');
     }
     onSelect([...newArray]);
   };
@@ -49,46 +46,3 @@ export default function PlayerHand({ cards, selected, onSelect }) {
 }
 
 PlayerHand.propTypes = propType;
-
-/**
-function PlayerHand({ cards, selected, onSelect }) {
- const handleClick = (index) => {
-   const newArray = selected.slice();
- 
-   if (selected.includes(index)) {
-     const indexOfSelected = selected.indexOf(index); // finds index in selected[] that  that contains the clicked card
-     newArray.splice(indexOfSelected, 1);
-     //   onSelect([...newArray]);
-   } else {
-       onSelect([...selected, index]);
-   }
-   // onSelect([...selected, index]); // spreading makes a copy
-   onSelect([...newArray]);
- };
-*/
-
-/* ON SELECT CALLBACK
- 
-- Fires the onSelect callback when a card is touched, with the indexes of the selected cards
- 
-- I would like the onSelect callback to be passed an array of selected card indexes
- 
-- I would like to be able to select, and deselect cards before submitting, so I can change my mind before committing to a decision
- 
- 
--A function that checks if that number is in the selected[] and if it is in the selected[] array remove it.
- 
-function updateSelection ([...]) {
-   checks if that number is in the selected[] and if it is in the selected[] array remove it else add it
-}
- 
-*/
-
-/* ON SELECT CALLBACK
-
-function updateSelection ([...]) {
-    if selected[] includes index new array with it sliced/spliced, 
-    else, new copy with it 
-}
-
-*/
