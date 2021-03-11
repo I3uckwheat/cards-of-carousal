f8b23c67393175468078f5384755a20d31f28f99
Tatiana <hello@tatianacodes.com>
2021-03-11 12:57:11 -0800

    update czar hand example with temporary styling

--- frontend/src/temp/CzarHandExample.js
+++ frontend/src/temp/CzarHandExample.js
@@ -1,7 +1,14 @@
 /* eslint-disable */
 import React, { useState } from 'react';
+import styled from 'styled-components';
 import CzarHand from '../components/CzarHand/CzarHand';
 
+const CzarExampleStyles = styled.div`
+  overflow-y: scroll;
+  display: flex;
+  flex-direction: row;
+  gap: 16px;
+`;
 const cards = [
   [
     "Card One has a REALLLLLLLLY long title and it's going to be rendered small asdfasdfasdfasdfasfasdfasfasdfasdfasfasdfasdfasdfasdfasdfasfasfd",
@@ -16,12 +23,12 @@ export default function CzarHandExample() {
   const [selectedCard, setSelectedCard] = useState(null);
 
   return (
-    <div>
+    <CzarExampleStyles>
       <CzarHand
         selectedGroup={selectedCard}
         cards={cards}
         onSelect={(selected) => setSelectedCard(selected)}
       />
-    </div>
+    </CzarExampleStyles>
   );
 }
