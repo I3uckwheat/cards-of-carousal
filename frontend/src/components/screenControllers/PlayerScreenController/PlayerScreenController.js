f23235ac7fa17b55083a9f20a30e040d4d11c401
sher-s7 <sher.sheikh77@gmail.com>
2021-03-13 22:55:40 -0500

    Return player screen wrapped in the player provider

--- frontend/src/components/screenControllers/PlayerScreenController/PlayerScreenController.js
+++ frontend/src/components/screenControllers/PlayerScreenController/PlayerScreenController.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import PlayerJoinScreen from '../../screens/PlayerJoinScreen/PlayerJoinScreen';
+import { PlayerProvider } from '../../../contexts/PlayerContext/PlayerContext';
+import PlayerScreen from '../PlayerScreen';
 
 const propTypes = {};
 
 export default function PlayerScreenController() {
-  return <PlayerJoinScreen />;
+  return (
+    <PlayerProvider>
+      <PlayerScreen />
+    </PlayerProvider>
+  );
 }
 
 PlayerScreenController.propTypes = propTypes;
