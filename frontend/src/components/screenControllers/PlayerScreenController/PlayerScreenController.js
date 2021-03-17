908730b1a386b43089deb523a7b5e9af4b7ba6a3
sher-s7 <sher.sheikh77@gmail.com>
2021-03-17 00:12:35 -0400

    Move PlayerScreen functionality into PlayerScreenController

--- frontend/src/components/screenControllers/PlayerScreenController/PlayerScreenController.js
+++ frontend/src/components/screenControllers/PlayerScreenController/PlayerScreenController.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import { PlayerProvider } from '../../../contexts/PlayerContext/PlayerContext';
-import PlayerScreen from './PlayerScreen';
+import React, { useContext } from 'react';
+import { PlayerContext } from '../../../contexts/PlayerContext/PlayerContext';
+import PlayerJoinScreen from '../../screens/PlayerJoinScreen/PlayerJoinScreen';
 
 const propTypes = {};
 
 export default function PlayerScreenController() {
-  return (
-    <PlayerProvider>
-      <PlayerScreen />
-    </PlayerProvider>
-  );
+  const playerContext = useContext(PlayerContext);
+  switch (playerContext.state.gameState) {
+    case 'enter-code':
+      return <PlayerJoinScreen />;
+    default:
+      throw new Error(
+        `Unrecognized game state: ${playerContext.state.gameState}`,
+      );
+  }
 }
 
 PlayerScreenController.propTypes = propTypes;
