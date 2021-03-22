5979a3a3b6b2168d48851c42a3b939a71f23bd78
Tatiana <hello@tatianacodes.com>
2021-03-21 21:07:11 -0700

    update test cases to reflect non-required selectedGroup prop

--- frontend/src/components/CzarHand/CzarHand.spec.js
+++ frontend/src/components/CzarHand/CzarHand.spec.js
@@ -61,17 +61,6 @@ describe('CzarHand', () => {
         }).toThrow();
       });
 
-      it('logs an error to the console when an undefined selectedGroup has been passed in', () => {
-        render(
-          <CzarHand
-            cards={cards}
-            selectedGroup={undefined}
-            onSelect={mockSelect}
-          />,
-        );
-        expect(consoleSpy).toHaveBeenCalled();
-      });
-
       it('logs an error to the console when no onSelect function has been passed in', () => {
         render(
           <CzarHand cards={cards} selectedGroup={0} onSelect={undefined} />,
