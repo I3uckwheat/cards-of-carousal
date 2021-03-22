289221b0147ae58723ee3fea5e176d80077f052d
Grace <grace.schwar@gmail.com>
2021-03-22 17:07:29 -0400

    Add case for PlayerScreenController
    - update App.js to work with provider to show PlayerHandScreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,13 +6,18 @@ import './App.css';
 import WelcomeScreen from './components/screens/WelcomeScreen/WelcomeScreen';
 import HostScreenController from './components/screenControllers/HostScreenController/HostScreenController';
 import PlayerScreenController from './components/screenControllers/PlayerScreenController/PlayerScreenController';
+import { PlayerProvider } from './contexts/PlayerContext/PlayerContext';
 
 function App() {
   const [screenControllerType, setScreenControllerType] = useState('welcome');
 
   switch (screenControllerType) {
     case 'player':
-      return <PlayerScreenController />;
+      return (
+        <PlayerProvider>
+          <PlayerScreenController />
+        </PlayerProvider>
+      );
     case 'host':
       return <HostScreenController />;
     case 'welcome':
--- frontend/src/components/screenControllers/PlayerScreenController/PlayerScreenController.js
+++ frontend/src/components/screenControllers/PlayerScreenController/PlayerScreenController.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { PlayerContext } from '../../../contexts/PlayerContext/PlayerContext';
 import PlayerJoinScreen from '../../screens/PlayerJoinScreen/PlayerJoinScreen';
+import PlayerHandScreen from '../../screens/PlayerHandScreen/PlayerHandScreen';
 
 const propTypes = {};
 
 export default function PlayerScreenController() {
-  return <PlayerJoinScreen />;
+  const {
+    state: { gameState },
+  } = useContext(PlayerContext);
+  switch (gameState) {
+    case 'enter-code':
+      return <PlayerJoinScreen />;
+    case 'player-selection':
+      return <PlayerHandScreen />;
+    default:
+      throw new Error(`Unrecognized game state: ${gameState}`);
+  }
 }
 
 PlayerScreenController.propTypes = propTypes;
