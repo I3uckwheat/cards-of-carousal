01f82675a6c3379d887bab9b4710a3085eeab821
Grace <grace.schwar@gmail.com>
2021-03-30 17:46:31 -0400

    Initial component creation and layout

---
+++ frontend/src/components/LoadingIndicator/LoadingInicator.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const propType = {
+  secondary: PropTypes.bool.isRequired,
+};
+
+const Loader = styled.div`
+  .loading {
+    display: inline-block;
+    position: relative;
+    width: 80px;
+    height: 80px;
+  }
+  .loading div {
+    display: inline-block;
+    position: absolute;
+    left: 8px;
+    width: 16px;
+    background: pink;
+    animation: loading 1.2s cubic-bezier(0, 0.5, 0.5, 1) infinite;
+  }
+  .loading div:nth-child(1) {
+    left: 8px;
+    animation-delay: -0.24s;
+  }
+  .loading div:nth-child(2) {
+    left: 32px;
+    animation-delay: -0.12s;
+  }
+  .loading div:nth-child(3) {
+    left: 56px;
+    animation-delay: 0;
+  }
+  @keyframes loading {
+    0% {
+      top: 8px;
+      height: 64px;
+    }
+    50%,
+    100% {
+      top: 24px;
+      height: 32px;
+    }
+  }
+`;
+
+export default function LoadingIndicator({ secondary }) {
+  if (secondary) {
+    console.log('hi');
+  }
+
+  return (
+    <Loader className="loading">
+      <div />
+      <div />
+      <div />
+    </Loader>
+  );
+}
+
+LoadingIndicator.propTypes = propType;
