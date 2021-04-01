2838c35bf05b1ac8c5bde98e43dd32fbb9730073
Grace <grace.schwar@gmail.com>
2021-04-01 13:46:07 -0400

    Create testing boilerplate

---
+++ frontend/src/components/LoadingIndicator/LoadingIndicator.spec.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import LoadingIndicator from './LoadingIndicator';
+
+describe('LoadingIndicator', () => {
+  describe('rendering', () => {
+    // snapshot
+    // renders in correct color when true
+    // renders in correct color when false
+    // renders in correct color when no prop
+  });
+
+  describe('propTypes', () => {
+    // logs an error if prop is not a boolean
+    // does not log an error if no prop is passed
+  });
+});
