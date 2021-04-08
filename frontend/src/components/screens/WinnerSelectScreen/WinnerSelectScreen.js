74e9f6dbdd5976564990709212db4784ca54c7a3
Pedro Klepa <pedro.klepa@gmail.com>
2021-04-08 23:28:03 +0100

    Update markup to follow standards for the application by using class names for inner divs

--- frontend/src/components/screens/WinnerSelectScreen/WinnerSelectScreen.js
+++ frontend/src/components/screens/WinnerSelectScreen/WinnerSelectScreen.js
@@ -33,40 +33,35 @@ const RightPanelWrapper = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: space-between;
-`;
 
-const CzarTitle = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin: 25px 0;
+  .czar-title {
+    display: flex;
+    flex-direction: column;
+    margin: 25px 0;
+    line-height: 1em;
 
-  span,
-  .czar-name {
+    font-size: 56px;
     font-weight: 900;
     text-transform: uppercase;
-    line-height: 1em;
+
+    span {
+      font-size: 24px;
+      text-indent: -20px;
+      margin-bottom: -8px;
+      line-height: 1em;
+    }
   }
 
-  span {
-    font-size: 24px;
-    text-indent: -16px;
-    margin-bottom: -8px;
+  .white-cards {
+    display: grid;
+    grid-template-columns: repeat(3, auto);
+    gap: 20px;
+    place-items: center;
+    justify-content: center;
+    padding: 40px 0;
+    width: 100%;
+    max-height: 250px;
   }
-
-  .czar-name {
-    font-size: 56px;
-  }
-`;
-
-const WhiteCards = styled.div`
-  display: grid;
-  grid-template-columns: repeat(3, auto);
-  gap: 20px;
-  place-items: center;
-  justify-content: center;
-  padding: 40px 0;
-  width: 100%;
-  max-height: 250px;
 `;
 
 function LeftPanel() {
@@ -96,22 +91,22 @@ function RightPanel() {
 
   return (
     <RightPanelWrapper>
-      <CzarTitle>
+      <h1 className="czar-title">
         <span>Czar:</span>
-        <h1 className="czar-name">{currentCzar.name}</h1>
-      </CzarTitle>
+        {currentCzar.name}
+      </h1>
 
       <BlackCard pickCount={state.selectedBlackCard.pick}>
         {state.selectedBlackCard.text}
       </BlackCard>
 
-      <WhiteCards>
+      <div className="white-cards">
         {highlightedPlayer?.submittedCards.map((cardIndex) => (
           <WhiteCard key={cardIndex}>
             {highlightedPlayer.cards[cardIndex]}
           </WhiteCard>
         ))}
-      </WhiteCards>
+      </div>
     </RightPanelWrapper>
   );
 }
