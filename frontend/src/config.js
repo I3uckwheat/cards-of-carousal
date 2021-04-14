ab7aab20688f68ba1225a7fc4bf0424840a7f3c0
Zach Meyer <zachary.michael.meyer@gmail.com>
2021-04-14 18:53:13 -0400

    Move config file into src folder

---
+++ frontend/src/config.js
@@ -0,0 +1,15 @@
+const configValues = {
+  maxPlayersValues: {
+    min: 2,
+    max: 12,
+    default: 5,
+  },
+
+  maxScoreValues: {
+    min: 1,
+    max: 15,
+    default: 5,
+  },
+};
+
+export default configValues;
