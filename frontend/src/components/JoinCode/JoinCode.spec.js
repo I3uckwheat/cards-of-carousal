a314f50198f85f352c5b37140fb8c89feb1ac088
sher-s7 <sher.sheikh77@gmail.com>
2021-04-15 18:53:51 -0400

    Adjust and add tests for JoinCode

--- frontend/src/components/JoinCode/JoinCode.spec.js
+++ frontend/src/components/JoinCode/JoinCode.spec.js
@@ -10,18 +10,26 @@ describe('joinCode', () => {
   describe('rendering', () => {
     it('renders when "code" is an empty string', () => {
       render(<DisplayJoinCode loading={[]} code="" />);
+      expect(screen.queryByTestId('loader')).not.toBeInTheDocument();
       expect(screen.getByTestId('join-code')).toBeInTheDocument();
     });
 
     it('renders properly', () => {
       render(<DisplayJoinCode loading={[]} code="XYA3Z" />);
+      expect(screen.queryByTestId('loader')).not.toBeInTheDocument();
       expect(screen.getByText('XYA3Z')).toBeInTheDocument();
     });
 
-    it('displays a loading indicator when the loading array includes the join-code string', () => {
+    it("displays a loading indicator when the loading array includes the 'join-code' string", () => {
       render(<DisplayJoinCode loading={['join-code']} code="XYA3Z" />);
       expect(screen.getByTestId('loader')).toBeInTheDocument();
       expect(screen.queryByText('XYA3Z')).not.toBeInTheDocument();
+    });
+
+    it("does not display a loading indicator when the loading array is non-empty and does not include the 'join-code' string", () => {
+      render(<DisplayJoinCode loading={['test']} code="XYA3Z" />);
+      expect(screen.queryByTestId('loader')).not.toBeInTheDocument();
+      expect(screen.queryByText('XYA3Z')).toBeInTheDocument();
     });
   });
 });
