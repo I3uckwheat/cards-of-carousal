1b559dc2a776eb9b62176a64878f0202c9d88671
sher-s7 <sher.sheikh77@gmail.com>
2021-04-14 21:22:18 -0400

    Add test to check if loading indicator is displayed when the loading array contains the join-code string

--- frontend/src/components/JoinCode/JoinCode.spec.js
+++ frontend/src/components/JoinCode/JoinCode.spec.js
@@ -17,5 +17,11 @@ describe('joinCode', () => {
       render(<DisplayJoinCode loading={[]} code="XYA3Z" />);
       expect(screen.getByText('XYA3Z')).toBeInTheDocument();
     });
+
+    it('displays a loading indicator when the loading array includes the join-code string', () => {
+      render(<DisplayJoinCode loading={['join-code']} code="XYA3Z" />);
+      expect(screen.getByTestId('loader')).toBeInTheDocument();
+      expect(screen.queryByText('XYA3Z')).not.toBeInTheDocument();
+    });
   });
 });
