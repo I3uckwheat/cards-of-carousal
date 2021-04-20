5fb83b2f5b9658f3cf1bd93cf8802955f09f001f
Pedro Klepa <pedro.klepa@gmail.com>
2021-04-20 19:51:34 +0100

    Add tests for game-states that render HostPregameScreen

--- frontend/src/components/screenControllers/HostScreenController/HostScreenController.spec.js
+++ frontend/src/components/screenControllers/HostScreenController/HostScreenController.spec.js
@@ -1,8 +1,15 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { HostContext } from '../../../contexts/HostContext/HostContext';
 
 import HostScreenController from './HostScreenController';
+import HostPregameScreen from '../../screens/HostPregameScreen/HostPregameScreen';
+
+const MockHostPregameScreen = () => <div data-testid="host-pregame-screen" />;
+jest.mock('../../screens/HostPregameScreen/HostPregameScreen.js', () => ({
+  __esModule: true,
+  default: jest.fn(),
+}));
 
 describe('Host screen controller', () => {
   describe('gameState switch', () => {
@@ -28,6 +35,74 @@ describe('Host screen controller', () => {
         // Restore writing to console.
         // eslint-disable-next-line no-console
         console.error = err;
+      });
+    });
+
+    describe('waiting-for-lobby', () => {
+      it('renders HostPregameScreen', () => {
+        HostPregameScreen.mockImplementation(MockHostPregameScreen);
+
+        const dispatch = jest.fn();
+        const state = { gameState: 'waiting-for-lobby' };
+
+        render(
+          <HostContext.Provider value={{ state, dispatch }}>
+            <HostScreenController />
+          </HostContext.Provider>,
+        );
+
+        expect(screen.getByTestId('host-pregame-screen')).toBeInTheDocument();
+      });
+    });
+
+    describe('waiting-for-players', () => {
+      it('renders HostPregameScreen', () => {
+        HostPregameScreen.mockImplementation(MockHostPregameScreen);
+
+        const dispatch = jest.fn();
+        const state = { gameState: 'waiting-for-players' };
+
+        render(
+          <HostContext.Provider value={{ state, dispatch }}>
+            <HostScreenController />
+          </HostContext.Provider>,
+        );
+
+        expect(screen.getByTestId('host-pregame-screen')).toBeInTheDocument();
+      });
+    });
+
+    describe('waiting-for-deck', () => {
+      it('renders HostPregameScreen', () => {
+        HostPregameScreen.mockImplementation(MockHostPregameScreen);
+
+        const dispatch = jest.fn();
+        const state = { gameState: 'waiting-for-deck' };
+
+        render(
+          <HostContext.Provider value={{ state, dispatch }}>
+            <HostScreenController />
+          </HostContext.Provider>,
+        );
+
+        expect(screen.getByTestId('host-pregame-screen')).toBeInTheDocument();
+      });
+    });
+
+    describe('waiting-for-player-card-submissions', () => {
+      it('renders HostPregameScreen', () => {
+        HostPregameScreen.mockImplementation(MockHostPregameScreen);
+
+        const dispatch = jest.fn();
+        const state = { gameState: 'waiting-for-player-card-submissions' };
+
+        render(
+          <HostContext.Provider value={{ state, dispatch }}>
+            <HostScreenController />
+          </HostContext.Provider>,
+        );
+
+        expect(screen.getByTestId('host-pregame-screen')).toBeInTheDocument();
       });
     });
   });
