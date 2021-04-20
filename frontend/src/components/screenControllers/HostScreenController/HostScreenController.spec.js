c810db59f5c2e14c9198fde4cef27ac9d9fcff96
Pedro Klepa <pedro.klepa@gmail.com>
2021-04-20 19:44:15 +0100

    Remove unnecessary snapshot tests and add one for the default state in HostScreenController

--- frontend/src/components/screenControllers/HostScreenController/HostScreenController.spec.js
+++ frontend/src/components/screenControllers/HostScreenController/HostScreenController.spec.js
@@ -1,21 +1,34 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
-import HostProvider from '../../../contexts/HostContext/HostContext';
+import { render } from '@testing-library/react';
+import { HostContext } from '../../../contexts/HostContext/HostContext';
 
 import HostScreenController from './HostScreenController';
 
 describe('Host screen controller', () => {
-  describe('render', () => {
-    it('renders correctly', () => {
-      const tree = renderer
-        .create(
-          <HostProvider>
-            <HostScreenController />
-          </HostProvider>,
-        )
-        .toJSON();
+  describe('gameState switch', () => {
+    describe('default', () => {
+      it('throws an error', () => {
+        // Prevent writing error in console during this render.
+        // eslint-disable-next-line no-console
+        const err = console.error;
+        // eslint-disable-next-line no-console
+        console.error = jest.fn();
 
-      expect(tree).toMatchSnapshot();
+        const dispatch = jest.fn();
+        const state = { gameState: '' };
+
+        expect(() =>
+          render(
+            <HostContext.Provider value={{ state, dispatch }}>
+              <HostScreenController />
+            </HostContext.Provider>,
+          ),
+        ).toThrowError();
+
+        // Restore writing to console.
+        // eslint-disable-next-line no-console
+        console.error = err;
+      });
     });
   });
 });
