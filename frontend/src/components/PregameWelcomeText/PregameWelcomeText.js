4b6ab78e5614e0a9ce3022df7586ce3f6a46be9d
Sully <a.m.sullivan.github@gmail.com>
2021-05-02 23:40:59 -0400

    Edit pregame intro/rules copy

--- frontend/src/components/PregameWelcomeText/PregameWelcomeText.js
+++ frontend/src/components/PregameWelcomeText/PregameWelcomeText.js
@@ -47,41 +47,6 @@ const StyledPregameWelcomeText = styled.div`
   }
 `;
 
-function Rules() {
-  return (
-    <>
-      <h2>Rules:</h2>
-      <p className="snarkyText">
-        (if you want to follow them, I&apos;m not your dad)
-      </p>
-      <ol>
-        <li>
-          Each turn one player will be the &quot;Czar&quot; and read the black
-          card (on this screen) out loud.
-        </li>
-        <li>
-          Everyone else submits their white card that fills the blank or answers
-          the black card in (what they think is, but probably isn&apos;t) the
-          funniest way. The number of cards submitted by each player must match
-          the &quot;PICK X&quot; number in the bottom right of the black card.
-        </li>
-        <li>
-          The Czar reads all of the submissions (black card + white card
-          together each time) out loud and picks the card they think is the
-          best; whoever submitted it gets one point.
-        </li>
-        <li>
-          Another player then becomes the new Czar and the process starts over.
-        </li>
-      </ol>
-      <div className="howToWin">
-        <h3>The first player to reach the winning score wins!</h3>
-        <p className="snarkyText">(duh)</p>
-      </div>
-    </>
-  );
-}
-
 function Welcome() {
   return (
     <>
@@ -100,10 +65,94 @@ function Welcome() {
   );
 }
 
+function Setup() {
+  return (
+    <>
+      <h2>How to play:</h2>
+      <ol>
+        <li>
+          All players get on a voice chat together / gather around a tv with a
+          web browser if in person
+        </li>
+        <li>
+          One player streams this screen to all the others / has this on the
+          shared screen
+        </li>
+        <li>
+          All players go to this site in a web browser, hit the join button, and
+          input the &quot;JOIN CODE&quot; shown in the bottom left of this
+          screen
+        </li>
+        <li>
+          The host selects the card packs to play with, what the winning score
+          should be, and sets a max player limit (if desired)
+        </li>
+        <li>
+          Once ready to start, the host clicks the &quot;START CAROUSING&quot;
+          button on this screen
+        </li>
+      </ol>
+    </>
+  );
+}
+
+function Rules1() {
+  return (
+    <>
+      <h2>Rules:</h2>
+      <p className="snarkyText">
+        (if you want to follow them, I&apos;m not your dad)
+      </p>
+      <ol>
+        <li>
+          Each round 1 player is selected as the Czar, and the other players
+          will be jesters
+        </li>
+        <li>A black card with a sentence will appear on this screen</li>
+        <li>
+          Jesters will each receive a hand of white cards with a unique
+          word/phrase
+        </li>
+        <li>The Czar reads the text on the black card out loud</li>
+        <li>
+          Jesters submit a number of their white cards equal to the &quot;PICK
+          X&quot; number on the bottom right of the black card
+        </li>
+      </ol>
+    </>
+  );
+}
+
+function Rules2() {
+  return (
+    <>
+      <h2>Rules 2 - Eccentric Caribou: </h2>
+      <ol start="6">
+        <li>
+          The Czar reads each submission out loud, in the context of the black
+          card
+        </li>
+        <li>
+          The Czar picks their favorite submission, the winning jester gets one
+          point
+        </li>
+        <li>Next player becomes Czar and a new round starts</li>
+        <div className="howToWin">
+          <h3>The first player to reach the winning score wins!</h3>
+          <p className="snarkyText">(duh)</p>
+        </div>
+      </ol>
+    </>
+  );
+}
+
 export default function PregameWelcomeText() {
   return (
     <StyledPregameWelcomeText>
-      <Carousel slides={[<Welcome />, <Rules />]} interval={2000} />
+      <Carousel
+        slides={[<Welcome />, <Setup />, <Rules1 />, <Rules2 />]}
+        interval={2000}
+      />
     </StyledPregameWelcomeText>
   );
 }
