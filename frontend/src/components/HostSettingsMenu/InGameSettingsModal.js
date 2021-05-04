06db87bdd72bddb0b2a50bcabbdfed590577c072
Tatiana <hello@tatianacodes.com>
2021-05-03 19:48:04 -0700

    adds SkipPlayerButton to host settings modal

--- frontend/src/components/HostSettingsMenu/InGameSettingsModal.js
+++ frontend/src/components/HostSettingsMenu/InGameSettingsModal.js
@@ -4,11 +4,13 @@ import HostSettingsMenu from './HostSettingsMenu';
 import PlayerKicker from './SettingsSubComponents/PlayerKicker';
 import JoinCodeHider from './SettingsSubComponents/JoinCodeHider';
 import JoinCodeShuffler from './SettingsSubComponents/JoinCodeShuffler';
+import SkipPlayerButton from './SettingsSubComponents/SkipPlayerButton';
 
 export default function PreGameSettingsModal() {
   return (
     <HostSettingsMenu
       settingsComponentList={[
+        { type: 'button', component: SkipPlayerButton },
         { type: 'button', component: JoinCodeHider },
         { type: 'accordion', component: PlayerKicker },
         { type: 'button', component: JoinCodeShuffler },
