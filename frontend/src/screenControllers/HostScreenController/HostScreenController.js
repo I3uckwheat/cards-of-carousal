539c3bf06b1224a9d7b6924d107fbc5aaff25e28
Sully <a.m.sullivan.github@gmail.com>
2021-05-22 05:02:00 -0400

    Add host minimum window size enforcement screen

--- frontend/src/screenControllers/HostScreenController/HostScreenController.js
+++ frontend/src/screenControllers/HostScreenController/HostScreenController.js
@@ -1,18 +1,40 @@
 import React, { useContext } from 'react';
+import styled from 'styled-components';
 import { HostContext } from '../../contexts/HostContext/HostContext';
 import HostPregameScreen from '../../screens/HostPregameScreen/HostPregameScreen';
 import WinnerSelectScreen from '../../screens/WinnerSelectScreen/WinnerSelectScreen';
 import HostBlackCardScreen from '../../screens/HostBlackCardScreen/HostBlackCardScreen';
 import HostRoundWinnerScreen from '../../screens/HostRoundWinnerScreen/HostRoundWinnerScreen';
 import HostGameOverScreen from '../../screens/HostGameOverScreen/HostGameOverScreen';
+import config from '../../config';
 
+const { smallDesktop } = config.breakpoint.hostBreakpoints;
 const propTypes = {};
 
-export default function HostScreenController() {
-  const {
-    state: { gameState },
-  } = useContext(HostContext);
+const ScreenTooSmall = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  height: 100%;
+  overflow: auto;
 
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  z-index: 10200;
+
+  background-color: var(--primary-background-color);
+
+  @media (min-width: ${smallDesktop}) and (min-height: 768px) {
+    display: none;
+  }
+`;
+
+function showScreen(gameState) {
   switch (gameState) {
     case 'waiting-for-lobby':
     case 'waiting-for-players':
@@ -35,6 +57,23 @@ export default function HostScreenController() {
     default:
       throw new Error(`Unrecognized game state: ${gameState}`);
   }
+}
+
+export default function HostScreenController() {
+  const {
+    state: { gameState },
+  } = useContext(HostContext);
+
+  return (
+    <>
+      <ScreenTooSmall>
+        <h1>Your screen size isn&apos;t big enough to host a lobby</h1>
+        <p>Sorry :(</p>
+      </ScreenTooSmall>
+
+      {showScreen(gameState)}
+    </>
+  );
 }
 
 HostScreenController.propTypes = propTypes;
