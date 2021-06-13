1ff0ae7a1956a4f07c66d37de0fdf3dfdc0995e7
Alex Younger <alexanderyounger@gmail.com>
2021-06-13 11:17:11 -0400

    revert start.js

--- backend/start.js
+++ backend/start.js
@@ -7,7 +7,7 @@ const app = require('./app/express.js');
 /* ======= Start socket service ======= */
 require('./app/socket.js');
 
-app.listen(process.env.EXPRESS_PORT, '0.0.0.0', () =>
+app.listen(process.env.EXPRESS_PORT, () =>
   // eslint-disable-next-line no-console
   console.log(
     `Express listening at http://localhost:${process.env.EXPRESS_PORT}`,
